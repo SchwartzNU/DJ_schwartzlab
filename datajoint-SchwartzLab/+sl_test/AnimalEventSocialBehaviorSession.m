d3f7af682a9e0399ec6803208ca7a9b7d991ecfd
Greg Schwartz <greg.schwartz@northwestern.edu>
2020-09-18 15:03:54 -0500

    fixin some stuff

--- datajoint-SchwartzLab/+sl_test/AnimalEventSocialBehaviorSession.m
+++ datajoint-SchwartzLab/+sl_test/AnimalEventSocialBehaviorSession.m
@@ -2,14 +2,17 @@
 # Session in 3 chamber circular social behavior device. Animal here is the TEST (center) animal
 -> sl_test.AnimalEvent                               # includes date of session
 --- 
--> sl_test.User.proj(experimenter='name')                    # who did it
+(experimenter) -> sl_test.User(name)                 # who did it
 recorded : tinyint unsigned                          # 0 = false or 1 = true
 fname : varchar(128)                                 # root filename if session was recorded
 session_time : time                                  # session time
 duration_mins : smallint unsigned                    # approximate duration (minutes)
 notes : varchar(256)                                 # notes about the animal's state and comfort level, other people involvd, etc. 
 social_behavior_session_tags : longblob              # extra tags
-purpose : varchar(64)                                # type of experiment, like social dominace, mate preference familiarity with rig, etc.  
+purpose : varchar(64)                                # type of experiment, like social dominance, mate preference familiarity with rig, etc.  
+(animal_in_A) -> sl_test.Animal(animal_id)  # animal at position A (null for empty)
+(animal_in_B) -> sl_test.Animal(animal_id)  # animal at position B (null for empty)
+(animal_in_C) -> sl_test.Animal(animal_id)  # animal at position C (null for empty)
 %}
 
 classdef AnimalEventSocialBehaviorSession < dj.Part
@@ -17,10 +20,3 @@ classdef AnimalEventSocialBehaviorSession < dj.Part
         master = sl_test.AnimalEvent
     end
 end
-
-% -> [nullable] sl_test.Animal(animal_in_A='animal_id')           # animal at position A (null for empty)
-% -> [nullable] sl_test.Animal(animal_in_B='animal_id')           # animal at position B (null for empty)
-% -> [nullable] sl_test.Animal(animal_in_C='animal_id')           # animal at position C (null for empty)
-
-%-> sl_test.User(experimenter='name')                 # who did it
-
