716bceb8bc5109f68e34d66ccb5749f05b1431e0
Greg Schwartz <greg.schwartz@northwestern.edu>
2020-09-21 15:28:59 -0500

    starting to get evvents working in logger GUI

---
+++ Datajoint_utils/add_animalEvent.m
@@ -0,0 +1,22 @@
+function [] = add_animalEvent(key, event_type)
+C = dj.conn;
+C.startTransaction
+try 
+    key_AnimalEvent.date = key.date;   
+    key_AnimalEvent.animal_id = key.animal_id;
+    eventsThisDate = sl_test.AnimalEvent & ['animal_id=' num2str(key.animal_id)] & ['date=' '"' key.date '"'];
+    if eventsThisDate.exists
+        key_AnimalEvent.event_id = max(fetchn(eventsThisDate, 'event_id')) + 1;
+    else
+        key_AnimalEvent.event_id = 1;
+    end
+    insert(sl_test.AnimalEvent, key_AnimalEvent);    
+    
+    key.event_id = key_AnimalEvent.event_id;
+    insert(eval(['sl_test.AnimalEvent' event_type]), key);    
+    disp('Insert successful');
+    C.commitTransaction;    
+catch
+    errordlg('Insert failed');
+    C.cancelTransaction;
+end
