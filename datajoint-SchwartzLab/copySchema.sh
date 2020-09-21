7f4f013a1b0d3deed08ee85c29c06b61547d94b0
Zach Jessen <z.jessen@gmail.com>
2020-09-21 15:39:09 -0400

    shell script to copy schema code

---
+++ datajoint-SchwartzLab/copySchema.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#usage: ./copySchema oldDir newDir
+
+#copy old directory to new one
+cp -r +$1 +$2
+
+#replace strings in old directory so they refer to new directory
+for f in $(find +$2);
+do
+  echo $f;
+  sed -i "s/$1/$2/g" $f;
+done
