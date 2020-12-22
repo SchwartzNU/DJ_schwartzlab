ce89f3d71d73ec6e5c9f703807c0aceb54d632c5
Greg Schwartz <greg.schwartz@northwestern.edu>
2020-12-22 10:55:55 -0600

    a feew GUI updates

--- datajoint-SchwartzLab/+sl/Project.m
+++ datajoint-SchwartzLab/+sl/Project.m
@@ -1,8 +1,8 @@
 %{
 #A list of lab projects  
-
 project_name : varchar(32)  #what is the project called?
-
+---
+description = NULL : varchar(512)    # explanation of this project
 %}
 
 classdef Project < dj.Lookup
