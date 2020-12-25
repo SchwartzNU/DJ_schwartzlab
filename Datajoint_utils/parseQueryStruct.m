b4c0a07c912a895a8da9b8bf17605c176ee4a9b9
Greg Schwartz <greg.schwartz@northwestern.edu>
2020-12-24 19:07:41 -0600

    made the query builder and parser

---
+++ Datajoint_utils/parseQueryStruct.m
@@ -0,0 +1,115 @@
+function q = parseQueryStruct(queryState)
+q = 'error';
+C = dj.conn;
+if ~C.isConnected
+    disp('DataJoint connection failed.');
+    return
+end
+
+%make searchTable
+currentTables = queryState.currentTables;
+commandStr = 'searchTable=';
+N = length(currentTables);
+for i=1:N
+    commandStr = [commandStr, 'sl.' currentTables{i} '*'];
+end
+%remove trailing *
+commandStr = [commandStr(1:end-1) ,';'];
+eval(commandStr);
+
+%TODO special case where logical comes first
+
+rowInd = 1:2:length(queryState.attributeColumn);
+
+for i=rowInd
+    curOp = queryState.operatorColumn(i).value;
+    curType = queryState.attributeColumn(i).type;
+    
+    curName = queryState.attributeColumn(i).name;
+    if strcmp(curType, 'date')
+        curValue = datestr(queryState.queryEntryColumn(i).value, 'yyyy-mm-dd');
+    else
+        curValue = queryState.queryEntryColumn(i).value;
+    end
+    
+    val = curValue;
+    switch curOp
+        case 'is'
+            op = '=';
+        case 'is not'
+            op = '!=';
+        case 'is like'
+            op = 'LIKE';
+        case 'is NULL'
+            op = '=';
+            val = NaN;
+        otherwise
+            op = curOp;
+    end
+    
+    if isnan(val)
+        entry{i} = [curName, op, '"', 'NULL', '"'];
+    elseif strcmp(op,'LIKE')
+        entry{i} = [curName, ' LIKE ', '"', val, '"'];
+    elseif ischar(val)
+        entry{i} = [curName, op, '"', val, '"'];
+    else
+        entry{i} = [curName, op, num2str(val)];
+    end
+end
+
+%now just parse the logic and run it!
+
+%first assemble
+Qstr = ['searchTable & ']; %TODO - leading NOT changes this
+for i=1:length(entry)
+    if ~isempty(entry{i})
+        Qstr = [Qstr, 'entry{' num2str(i) '} '];
+    end
+    txt = queryState.logicButtonColumn(i+1).text;
+    if ~isempty(txt)
+        txt = strtrim(txt);
+        switch txt
+            case 'AND'
+                Qstr = [Qstr, '& '];
+            case 'AND NOT'
+                %special case for this because it needs to be a separate clause
+                Qstr = ['( ', Qstr ' )', ' - '];
+            case ') AND NOT'
+                %special case for this because it needs to be a separate clause
+                Qstr = ['( ', Qstr '} )', ' - '];
+            case ') AND NOT ANY OF ('
+                %special case for this because it needs to be a separate clause
+                Qstr = ['( ', Qstr '} )', ' - {'];
+            case 'AND NOT ANY OF ('
+                %special case for this because it needs to be a separate clause
+                Qstr = ['( ', Qstr ' )', ' - {'];    
+            case 'AND ANY OF ('
+                Qstr = [Qstr, '& { '];
+            case 'ANY OF ('
+                Qstr = [Qstr, '{ '];
+            case ')'
+                Qstr = [Qstr, '} '];
+            case ') AND'
+                Qstr = [Qstr, '} & '];
+            case ') AND ANY OF ('
+                Qstr = [Qstr, '} & { '];
+            case ','
+                Qstr = [Qstr, ', '];
+        end
+    end
+end
+
+try
+    q = eval(Qstr);
+    C.close();
+catch ME
+    C.close();
+    disp('Parse error for string: ');
+    disp(Qstr);
+    rethrow(ME);
+end
+
+
+
+
