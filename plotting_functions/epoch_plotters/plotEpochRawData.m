c0abadf53f3f85cd837dcc34a44b55c628af629e
Greg Schwartz <greg.schwartz@northwestern.edu>
2021-01-23 16:53:37 -0600

    bug fix and doc

--- plotting_functions/epoch_plotters/plotEpochRawData.m
+++ plotting_functions/epoch_plotters/plotEpochRawData.m
@@ -1,15 +1,12 @@
 function tableData = plotEpochRawData(R,entry,ax)
 tableData = [];
 [timeAxis, data] = epochRawData(entry.cell_id, entry.epoch_number);
-plot(ax, timeAxis, data);
+plot(ax, timeAxis, data, 'k');
 xlabel(ax,'Time (s)');
 %look for spike data
-spResult = sl.SpikeTrain & entry;
+spResult = sl_mutable.SpikeTrain & entry;
 if spResult.count == 1
     sp = spResult.fetch1('sp');
-%     dt = timeAxis(2) - timeAxis(1);
-%     preTime_s = timeAxis(1);
-%sp = sp * dt - preTime_s;
     hold(ax,'on');
     scatter(ax, timeAxis(sp), data(sp), 'rx');
     hold(ax,'off');
