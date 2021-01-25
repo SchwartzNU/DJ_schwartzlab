6142b3068a686d72d51ea20d200bd2a90d66dbb0
Greg Schwartz <greg.schwartz@northwestern.edu>
2021-01-24 21:31:29 -0600

    support for DJ 3.4.1 in AnimalEvent among other things

--- schemas/+sl/AnimalEventBrainInjection.m
+++ schemas/+sl/AnimalEventBrainInjection.m
@@ -7,15 +7,13 @@ event_id : int unsigned auto_increment
 -> sl.User                          # who did the injection
 -> sl.BrainArea                     # targeted brain area
 hemisphere: enum('Left', 'Right')    # left or right side
-date: date
-time: time                           # time of 
+date: date                           # date of injections 
+time: time                    # time of day
 entry_time = CURRENT_TIMESTAMP : timestamp # when this was entered into db
-
 head_rotation : float                # degrees, if not straight down
 coordinates: longblob                # 3 element vector of coordinates in the standard order (AP, ML, DV)
 dilution: float                      # dilution of substance (or 0 if not applicable or non-diluted)
 notes = NULL: varchar(256)           # surgery notes (can include people who assisted)
-
 %}
 classdef AnimalEventBrainInjection < sl.AnimalEvent & dj.Manual
 
