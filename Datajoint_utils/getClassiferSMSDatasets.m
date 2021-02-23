db3192ca3d47c8da094e370196cd7f9562b72561
Greg Schwartz <greg.schwartz@northwestern.edu>
2021-02-23 07:30:22 -0600

    Update getClassiferSMSDatasets.m

--- Datajoint_utils/getClassiferSMSDatasets.m
+++ Datajoint_utils/getClassiferSMSDatasets.m
@@ -15,31 +15,14 @@ for i=1:L
     curType = fetch1(sl_mutable.CurrentCellType & SMS_all_struct(i), 'cell_type');
     ind = find(strcmp(all_types,curType));
     if ~isempty(ind)
-        thisDS = fetch(sl.Dataset & SMS_all_struct(i), 'epoch_ids');
         R = fetch1(sl_greg.DatasetResult & 'pipeline_name="typology_SMS_CA"' & SMS_all_struct(i), 'result');
                 
         curLabel = all_labels{ind};
         s = rmfield(SMS_all_struct(i), {'pipeline_name', 'dataset_func_name'});
         s.SpotSizeVec = R.spotSize;
         s.label = curLabel;
-        
-        Nsizes = length(R.spotSize);
-        
-        for j=1:Nsizes
-%             R.key_ind(j,:)
-%             thisDS.epoch_ids
-thisDS.cell_id
-thisDS.epoch_ids(logical(R.key_ind(j,:)))
-keyboard;
-            [psth_x, psth_y] = psth(thisDS.cell_id, thisDS.epoch_ids(logical(R.key_ind(j,:))), binSize, [], [], [], thisDS.channel);
-            if j==1
-                Nbins = length(psth_x);
-                SMS_PSTH = zeros(Nsizes, Nbins);
-                s.PSTH_X = psth_x;
-            end
-            SMS_PSTH(j,:) = psth_y;
-        end
-        s.SMS_PSTH = SMS_PSTH;
+        s.PSTH_X = R.psth_x;
+        s.SMS_PSTH = R.sms_psth;
         
         if z==1
             classifier_data = s;
