ea4c3bc225c1f730c191173aeb63ce164839d02e
zfj1 <39531155+zfj1@users.noreply.github.com>
2021-03-10 21:08:19 -0600

    project out sp

--- Datajoint_utils/populateAll.m
+++ Datajoint_utils/populateAll.m
@@ -1,5 +1,5 @@
 function [] = populateAll()
     parpopulate(sl.SymphonyRecordedCell);
-    parpopulate((sl_mutable.SpikeTrain & (sl.Epoch & "recording_mode='Cell attached'") & "channel=1") - sl_mutable.SpikeTrainMissing);
-    parpopulate((sl_mutable.SpikeTrain & (sl.Epoch & "recording2_mode='Cell attached'") & "channel=2") - sl_mutable.SpikeTrainMissing)
+    parpopulate((sl_mutable.SpikeTrain & (sl.Epoch & "recording_mode='Cell attached'") & "channel=1") - sl_mutable.SpikeTrainMissing().proj());
+    parpopulate((sl_mutable.SpikeTrain & (sl.Epoch & "recording2_mode='Cell attached'") & "channel=2") - sl_mutable.SpikeTrainMissing().proj())
 end
