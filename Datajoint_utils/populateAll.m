0b7848d154737fac3355b1b4341d08053587b3b3
zfj1 <39531155+zfj1@users.noreply.github.com>
2021-03-10 20:16:38 -0600

    switching to parpopulate

--- Datajoint_utils/populateAll.m
+++ Datajoint_utils/populateAll.m
@@ -1,4 +1,5 @@
 function [] = populateAll()
-    populate(sl.SymphonyRecordedCell);
-    populate(sl_mutable.SpikeTrain);
+    parpopulate(sl.SymphonyRecordedCell);
+    parpopulate(sl_mutable.SpikeTrain & (sl.Epoch & "recording_mode='Cell attached'") & "channel=1" - sl_mutable.SpikeTrainMissing);
+    parpopulate(sl_mutable.SpikeTrain & (sl.Epoch & "recording2_mode='Cell attached'") & "channel=2" - sl_mutable.SpikeTrainMissing)
 end
