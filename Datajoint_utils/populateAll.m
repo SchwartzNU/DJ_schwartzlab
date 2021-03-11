1bf55c36e0975418fd1845b172758917d7b2f128
Zach Jessen <z.jessen@gmail.com>
2021-03-10 23:16:58 -0600

    cleaned up

--- Datajoint_utils/populateAll.m
+++ Datajoint_utils/populateAll.m
@@ -1,12 +1,4 @@
 function [] = populateAll()
-    %parpopulate(sl.SymphonyRecordedCell);
-
-    % f = fetch(sl.Epoch & "recording_mode='Cell attached'");
-    % [f.channel] = deal(1);
-
-    % f2 = fetch(sl_mutable.SpikeTrainMissing);
-    % for n=1:numel(f2)
-    %     f   (arrayfun(@(x) isequaln(x, f2(n)), f)) = [];
-    % end
+    parpopulate(sl.SymphonyRecordedCell);
     parpopulate(sl_mutable.SpikeTrain);
 end
