3c2cbcc2b760c3544a55028bc30b2891d1835ea5
Greg Schwartz <greg.schwartz@northwestern.edu>
2021-03-19 16:34:28 -0500

    fixed bug with logging of substances

--- schemas/+sl/AnimalEventEyeInjection.m
+++ schemas/+sl/AnimalEventEyeInjection.m
@@ -16,8 +16,8 @@ notes: varchar(256)                  # injection notes (can include people who a
 classdef AnimalEventEyeInjection < sl.AnimalEvent & dj.Manual
         
     properties
-        printStr = '%s %s: Animal %d had a %s eye injection of %s, dilluted 1:%d, performed by %s. (%s)\n';
-        printFields = {'date', 'time', 'animal_id', 'side', 'substance_name', 'dilution', 'user_name', 'notes'};
+        printStr = '%s %s: Animal %d had a %s eye injection of substancce with id: %d, dilluted 1:%d, performed by %s. (%s)\n';
+        printFields = {'date', 'time', 'animal_id', 'side', 'substance_id', 'dilution', 'user_name', 'notes'};
     end
     
 end
