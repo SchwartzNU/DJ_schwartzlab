26c78588bdb6d98ff5b45210628ad6fe7772d89e
Greg Schwartz <greg.schwartz@northwestern.edu>
2021-07-14 15:52:04 -0500

    Update populateAll.m

--- Datajoint_utils/populateAll.m
+++ Datajoint_utils/populateAll.m
@@ -1,6 +1,6 @@
 function [] = populateAll()
-    %parpopulate(sl.SymphonyRecordedCell);
-    %parpopulate(sl_mutable.SpikeTrain);
-    populate(sl.SymphonyRecordedCell);
-    populate(sl_mutable.SpikeTrain);
+    parpopulate(sl.SymphonyRecordedCell);
+    parpopulate(sl_mutable.SpikeTrain);
+    %populate(sl.SymphonyRecordedCell);
+    %populate(sl_mutable.SpikeTrain);
 end
