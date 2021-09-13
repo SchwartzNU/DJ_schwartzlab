ab09a71717836b73f93d479bb2b8d60617bae1ae
zfj1 <39531155+zfj1@users.noreply.github.com>
2021-09-13 13:50:36 -0500

    override dataset insert method

--- schemas/+sln_symphony/Dataset.m
+++ schemas/+sln_symphony/Dataset.m
@@ -4,4 +4,28 @@
 dataset_name: varchar(64)
 %}
 classdef Dataset < dj.Manual
+    methods
+        
+        function insert(self, key)
+            transacted = false;
+            if self.schema.conn.inTransaction
+                transacted = true;
+            else
+                self.schema.conn.startTransaction;
+            end
+            try
+                insert@dj.Manual(self, rmfield(key, {'cells','epoch_channels'}));
+                sln_symphony.DatasetCell().insert(key.cells);
+                sln_symphony.DatasetEpochChannel().insert(key.epoch_channels);                
+            catch ME
+                if ~transacted
+                    self.schema.conn.cancelTransaction;
+                end
+                rethrow(ME);
+            end
+            if ~transacted
+                self.schema.conn.commitTransaction;
+            end
+        end
+    end
 end
