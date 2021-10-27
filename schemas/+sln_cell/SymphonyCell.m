59e93495decb31014e771a13711e98eb9b7dd5a2
zfj1 <39531155+zfj1@users.noreply.github.com>
2021-10-27 16:13:31 -0500

    basic support for cell typing with new database

---
+++ schemas/+sln_cell/SymphonyCell.m
@@ -0,0 +1,11 @@
+%{
+# A link to cells in symphony
+-> sln_cell.RetinalCell
+---
+-> sln_symphony.ExperimentCell
+
+%}
+classdef SymphonyCell < dj.Manual
+end
+
+%TODO: must verify that the symphony cell is from the right animal, eye, location!
---
+++ schemas/+sln_cell/Type.m
@@ -0,0 +1,6 @@
+%{
+# A list of cell type names
+type : varchar(32)
+%}
+classdef Type < dj.Manual
+end
