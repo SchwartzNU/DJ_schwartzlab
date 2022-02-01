40180891d1b9a3c102378acd3b90fff5bdf6c6ee
Zach Jessen <z.jessen@gmail.com>
2022-02-01 09:39:59 -0600

    small updates

--- schemas/+sln_symphony/ExperimentProtocolMovingObjectV1BlockParameters.m
+++ schemas/+sln_symphony/ExperimentProtocolMovingObjectV1BlockParameters.m
@@ -39,17 +39,27 @@ classdef ExperimentProtocolMovingObjectV1BlockParameters < sln_symphony.Experime
 				%min speed, max speed, number of speeds
 				min_speed = arrayfun(@(x) x.set_speeds(1), block_key,'uni',0);
 				max_speed = arrayfun(@(x) x.set_speeds(2), block_key,'uni',0);
-				num_speed = arrayfun(@(x) x.set_speeds(3), block_key,'uni',0);
+                has_num = arrayfun(@(x) numel(x.set_speeds)==3, block_key);
+                num_speed = ones(size(min_speed));
+                num_speed(has_num) = arrayfun(@(x) x.set_speeds(3), block_key(has_num));
+                num_speed = num2cell(num_speed);
 
 				%min offset, max offset, number of offsets
 				min_offset = arrayfun(@(x) x.set_offsets(1), block_key,'uni',0);
 				max_offset = arrayfun(@(x) x.set_offsets(2), block_key,'uni',0);
-				num_offset = arrayfun(@(x) x.set_offsets(3), block_key,'uni',0);
-
+				has_num = arrayfun(@(x) numel(x.set_offsets)==3, block_key);
+                num_offset = ones(size(min_offset));
+                num_offset(has_num) = arrayfun(@(x) x.set_offsets(3), block_key(has_num));
+                num_offset = num2cell(num_offset);
+                
+                
 				%min diameter, max diameter, number of diameters
 				min_diameter = arrayfun(@(x) x.set_diameters(1), block_key,'uni',0);
 				max_diameter = arrayfun(@(x) x.set_diameters(2), block_key,'uni',0);
-				num_diameter = arrayfun(@(x) x.set_diameters(3), block_key,'uni',0);
+				has_num = arrayfun(@(x) numel(x.set_diameters)==3, block_key);
+                num_diameter = ones(size(min_diameter));
+                num_diameter(has_num) = arrayfun(@(x) x.set_diameters(3), block_key(has_num));
+                num_diameter = num2cell(num_diameter);
 
 				[block_key(:).min_speed] = min_speed{:};
 				[block_key(:).max_speed] = max_speed{:};
