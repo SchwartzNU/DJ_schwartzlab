24eb330297316e27977a04725cdaab96024f5442
SchwartzLabNU <SchwartzLabNU@gmail.com>
2022-02-02 11:23:45 -0600

    bug

--- Datajoint_utils/H5Utils/loadSymphony2.cpp
+++ Datajoint_utils/H5Utils/loadSymphony2.cpp
@@ -291,7 +291,7 @@ class Parser {
         
         DEBUGPRINT("Reading symphony resource into " << n_samples << " bytes");
         DEBUGPRINT("Actual size: " << space.getSimpleExtentNpoints() << " points, (is simple? " << space.isSimple() << " ), " << space.getSimpleExtentNdims() << " dims");
-        DEBUGPRINT("Reading as '" << H5::PredType::STD_U8LE << "' (std_u8le)");
+        // DEBUGPRINT("Reading as '" << H5::PredType::STD_U8LE << "' (std_u8le)");
         ds.read(buffer.get(), H5::PredType::STD_U8LE);
         DEBUGPRINT("Done reading symphony resource");
         
