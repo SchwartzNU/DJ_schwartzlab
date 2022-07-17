cf212fe70149bd0f55ca0a5972d4b2b14052e0bf
Greg Schwartz <greg.schwartz@northwestern.edu>
2022-07-17 13:32:35 -0500

    Update insert.m

--- schemas/+sln_results/insert.m
+++ schemas/+sln_results/insert.m
@@ -14,9 +14,9 @@ try
     if ~isempty(msg)
         error('You have locally modified files in %s. Please commit them first.', getenv('DJ_ROOT'));
     end
-    tag_name = sprintf('%s: %s', cur_user, datestr(datetime('now')));
+    tag_name = sprintf('%s_ %s', cur_user, datestr(datetime('now'), 'yyyy-mmmm-dd-HH-MM-SS'));
     sprintf('git tag %s', tag_name)
-    system(sprintf('git tag "%s"', tag_name));
+    system(sprintf('git tag %s', tag_name));
     return;
 catch ME
     cd(cur_dir);
