429865d5c22edeed4e0bb3f83300a634cf06eb60
Greg Schwartz <greg.schwartz@northwestern.edu>
2022-07-18 17:44:04 -0500

    Create insert_all_datasets_for_experiment.m

---
+++ schemas/+sln_symphony/insert_all_datasets_for_experiment.m
@@ -0,0 +1,22 @@
+function [] = insert_all_datasets_for_experiment(file_name)
+CELL_DATA_FOLDER = getenv('CELL_DATA_FOLDER');
+cellNames = ls([CELL_DATA_FOLDER filesep file_name '*.mat']);
+if ispc
+    cellNames = cellstr(cellNames);
+else
+    cellNames = strsplit(cellNames); %this will be different on windows - see doc ls
+end
+
+cellBaseNames = cell(length(cellNames), 1);
+for i=1:length(cellNames)
+    [~, basename, ~] = fileparts(cellNames{i});
+    cellBaseNames{i} = basename;
+end
+
+for i=1:length(cellBaseNames)
+    if ~isempty(cellBaseNames{i})
+        fprintf('inserting datasets for cell %s\n', cellBaseNames{i});
+        insert(sln_symphony.Dataset,cellBaseNames{i});
+    end
+end
+
