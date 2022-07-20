00815d595ea4e9240e0286a9683cc8f4b3044080
Greg Schwartz <greg.schwartz@northwestern.edu>
2022-07-20 09:32:10 -0500

    utility functions for insert experiment

--- schemas/+sln_cell/init_cells_from_ExperimentCells.m
+++ schemas/+sln_cell/init_cells_from_ExperimentCells.m
@@ -1,4 +1,5 @@
 exp_cells = sln_symphony.ExperimentCell * proj(sln_symphony.ExperimentRetina,'*','source_id->retina_source_id') * proj(sln_animal.Animal,'*','source_id->animal_source_id');
-exp_cells_struct = rmfield(fetch(exp_cells),'retina_source_id');
+exp_cells_to_insert = exp_cells - sln_cell.Cell;
+exp_cells_struct = rmfield(fetch(exp_cells_to_insert),'retina_source_id');
 insert(sln_cell.Cell,exp_cells_struct);
 
