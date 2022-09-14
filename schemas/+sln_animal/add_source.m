b61a7e5980fcc15f1b81391269532fd722f5142d
Greg Schwartz <greg.schwartz@northwestern.edu>
2022-09-14 18:23:17 -0500

    updated add_source

--- schemas/+sln_animal/add_source.m
+++ schemas/+sln_animal/add_source.m
@@ -11,14 +11,31 @@ for i=1:length(keys)
         C = dj.conn;
         C.startTransaction;
         try
-            %source_id = add_source_if_missing(...)
-            %need to do this the hack way
-            %TODO: change this so that source numbers go according to
-            %Zach's scheme
-            query = 'INSERT INTO `sln_animal`.`source` (source_id) VALUES (null)';
-            C.query(query);
-            source_id(i) = fetch1(sln_animal.Source, 'source_id', 'ORDER BY source_id DESC LIMIT 1');
-            key.source_id = source_id(i);
+            current_source_ids = fetchn(sln_animal.Source,'source_id');
+            switch source_type
+                case 'Vendor' %Sources 1-89
+                    last_id = max(current_source_ids(current_source_ids<=89));
+                    if last_id == 89
+                       error('Error, ran out of Vendor IDs');
+                    end
+
+                case 'GenotypeSource' %Sources 90-99
+                    last_id = max(current_source_ids(current_source_ids>=90 & current_source_ids<=99));
+                    if last_id == 99
+                       error('Error, ran out of Genotype IDs');
+                    end
+
+                case 'Collaborator' %Sources 100-999
+                    last_id = max(current_source_ids(current_source_ids>=100 & current_source_ids<=999));
+                    if last_id == 999
+                       error('Error, ran out of Collaborator IDs');
+                    end
+
+                case 'BreedingPair' %Sources 1000+
+                    last_id = max(current_source_ids(current_source_ids>=1000));
+
+            end
+            key.source_id = last_id + 1;
             insert(feval(sprintf('sln_animal.%s',source_type)), key);
             C.commitTransaction;
         catch ME            
