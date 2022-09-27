2c2b19edf9c2f57de57af24d3b63467f8634b0df
Greg Schwartz <greg.schwartz@northwestern.edu>
2022-09-27 10:56:27 -0500

    working on multiPulse analysis stuff and adding ColorIso protocol to loader

---
+++ schemas/+sln_symphony/ExperimentProtocolColorIsoResponseV1BlockParameters.m
@@ -0,0 +1,32 @@
+%{
+#Block parameters for ContrastResponse (1) 
+-> sln_symphony.ExperimentEpochBlock
+---
+annulusMode : enum('true','false')
+annulusInnerDiameter : float
+annulusOuterDiameter : float
+pre_time : float
+rstar_mean : float
+spot_diameter : float
+stim_time : float
+tail_time : float
+intensity1: float
+intensity2: float
+contrast1: float
+contrast2: float
+%}
+classdef ExperimentProtocolColorIsoResponseV1BlockParameters < sln_symphony.ExperimentProtocol
+	properties
+
+		%attributes to be renamed
+		renamed_attributes = struct();
+
+		%attributes to be removed from the key
+		dropped_attributes = {'sessionId'};
+	end
+	methods
+		function block_key = add_attributes(self, block_key, epoch_key) %#ok<INUSL,INUSD>
+		%add entities to the key based on others
+		end
+	end
+end
