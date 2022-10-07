155515e9f58530c2f8ba4f0805c1694e6fb01e6b
Greg Schwartz <greg.schwartz@northwestern.edu>
2022-10-07 14:32:55 -0500

    TissueDB changes

--- schemas/+sln_tissue/StainReagent.m
+++ schemas/+sln_tissue/StainReagent.m
@@ -2,8 +2,9 @@
 #A reagent for tissue staining: primary, secondary, flurophore conjugate, etc.
 reagent_id : int unsigned AUTO_INCREMENT     # unique id
 ---
-suggested_dilution : int unsigned # 1:X dilution
+suggested_dilution = NULL : varchar(32) # 1:X dilution
 reagent_name : varchar(64)  #reagent name
+catalog_name : varchar(128) #name in catalog to search online
 -> sln_tissue.Vendor   # vendor for this reagent
 catalog_number : varchar(32)    # catalog number
 reagent_info = NULL : varchar(256) # additional information about the reagent
