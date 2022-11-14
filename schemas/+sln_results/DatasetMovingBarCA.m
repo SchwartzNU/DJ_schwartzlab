8f528d3113987acc43ca771263f97046fdb11455
Greg Schwartz <greg.schwartz@northwestern.edu>
2022-11-14 13:27:46 -0600

    Update DatasetMovingBarCA.m

--- schemas/+sln_results/DatasetMovingBarCA.m
+++ schemas/+sln_results/DatasetMovingBarCA.m
@@ -5,7 +5,7 @@ dataset_name : varchar(128) # dataset name
 source_id : int unsigned # source id used to identify the cell to which the dataset belongs
 ---
 -> sln_lab.User # user who entered this result
-bar_angles : longblob # set of bar angles (degrees)
+bar_angles=NULL : longblob # set of bar angles (degrees)
 bar_distance : int unsigned # travel distance (microns)
 bar_length : int unsigned # bar length (microns)
 bar_speed : int unsigned # bar speed (microns per second)
@@ -19,19 +19,19 @@ dsi_leading=NULL : float # vector sum dsi for the leading period
 dsi_trailing=NULL : float # vector sum dsi for the trailing period
 entry_time = CURRENT_TIMESTAMP : timestamp # time the result was entered
 git_tag : varchar(128) # git tag of current version of DJ_ROOT folder
-leading_trailing_index : longblob # spike count index defined as (leading - trailing) / (leading + trailing)
-n_epochs_per_angle : longblob # vector with how many trials for each angle
-peak_rate_leading : longblob # peak firing rate during the first half of the movement (Hz)
-peak_rate_trailing : longblob # peak firing rate during the second half of the movement (Hz)
+leading_trailing_index=NULL : longblob # spike count index defined as (leading - trailing) / (leading + trailing)
+n_epochs_per_angle=NULL : longblob # vector with how many trials for each angle
+peak_rate_leading=NULL : longblob # peak firing rate during the first half of the movement (Hz)
+peak_rate_trailing=NULL : longblob # peak firing rate during the second half of the movement (Hz)
 pre_time_ms : int unsigned # time before stimulus onset (ms)
-psth_by_angle : longblob # psth image by bar angle with 10 ms bins or other binning if specified in params
-psth_x : longblob # x (time) values for psth (seconds)
-spikes_full_mean : longblob # spike count during bar movement mean
-spikes_full_sem : longblob # spike count during bar movement sem
-spikes_leading_mean : longblob # spike count during first half of bar movement (leading edge), mean
-spikes_leading_sem : longblob # spike count during first half of bar movement (leading edge), sem
-spikes_trailing_mean : longblob # spike count during second half of bar movement (trailing edge), mean
-spikes_trailing_sem : longblob # spike count during second half of bar movement (trailing edge), sem
+psth_by_angle=NULL : longblob # psth image by bar angle with 10 ms bins or other binning if specified in params
+psth_x=NULL : longblob # x (time) values for psth (seconds)
+spikes_full_mean=NULL : longblob # spike count during bar movement mean
+spikes_full_sem=NULL : longblob # spike count during bar movement sem
+spikes_leading_mean=NULL : longblob # spike count during first half of bar movement (leading edge), mean
+spikes_leading_sem=NULL : longblob # spike count during first half of bar movement (leading edge), sem
+spikes_trailing_mean=NULL : longblob # spike count during second half of bar movement (trailing edge), mean
+spikes_trailing_sem=NULL : longblob # spike count during second half of bar movement (trailing edge), sem
 stim_time_ms : int unsigned # stimulus presentation time (ms)
 tail_time_ms : int unsigned # time after stimulus offset (ms)
 %}
