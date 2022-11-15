dc0d94cffa4ea7728f24a8fe4e18f3c3b71f86ac
Greg Schwartz <greg.schwartz@northwestern.edu>
2022-11-15 12:15:37 -0600

    plotting updates

---
+++ DJ_plotters_Greg/plot_EpochRawData.m
@@ -0,0 +1,32 @@
+function plot_EpochRawData(R,ax)
+ep_struct = fetch(sln_symphony.ExperimentEpochChannel * ...
+    sln_symphony.ExperimentChannel * ...
+    sln_symphony.ExperimentElectrode ...
+    & R, '*');
+data = ep_struct.raw_data;
+L = length(data);
+timeAxis = (1:L) / ep_struct.sample_rate;
+plot(ax, timeAxis, data, 'k');
+xlabel(ax,'Time (s)');
+set(ax,'XtickMode','auto');
+% %look for spike data
+spike_train = sln_symphony.SpikeTrain & R;
+if spike_train.count == 1
+    sp = fetch1(spike_train,'spike_indices');
+    hold(ax,'on');
+    scatter(ax, timeAxis(sp), data(sp), 'rx');
+    hold(ax,'off');
+end
+if strcmp(ep_struct.amp_mode,'Cell attached')
+    set(ax,'Ytick',[]);
+    ylabel(ax,'');
+else
+    set(ax,'YtickMode','auto');
+    if strcmp(ep_struct.recording_mode,'Current clamp')
+         ylabel(ax,'mV');
+    else
+        ylabel(ax,'pA');
+    end
+end
+
+
---
+++ DJ_plotters_Greg/plot_SMS_CA.m
@@ -0,0 +1,21 @@
+function plot_SMS_CA(R,ax)
+set(ax, 'XLim',[0 inf]);
+errorbar(ax, R.spot_sizes, R.spikes_stim_mean, R.spikes_stim_sem,...
+    'Color',[0 1 1],...
+    'LineWidth',2);
+set(ax,'XtickMode','auto');
+set(ax,'YtickMode','auto');
+hold(ax,'on');
+xlabel(ax, 'Spot size (microns)')
+ylabel(ax, 'Spike count from baseline');
+errorbar(ax, R.spot_sizes, R.spikes_tail_mean, R.spikes_tail_sem,...
+    'Color',[0 0 0],...
+    'LineWidth',2);
+hold(ax,'off');
+
+% tableData.RstarMean = R.RstarMean;
+% tableData.Intensity = R.RstarIntensity1;
+% tableData.bestSize_ON = R.bestSize_ON;
+% tableData.SuppIndexON = R.SI_ON;
+% tableData.bestSize_OFF = R.bestSize_OFF;
+% tableData.SuppIndexOFF = R.SI_OFF;
