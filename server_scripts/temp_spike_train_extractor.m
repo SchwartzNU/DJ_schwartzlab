ab8d3ea44537d653fa736bec54a4562026c239f1
Greg Schwartz <greg.schwartz@northwestern.edu>
2022-11-26 15:59:00 -0600

    Update temp_spike_train_extractor.m

--- server_scripts/temp_spike_train_extractor.m
+++ server_scripts/temp_spike_train_extractor.m
@@ -18,7 +18,8 @@ while z < N_trains
         sp_trains(i).cell_type = spike_trains_meta_data(z).cell_type;
         sp_trains(i).sample_rate = spike_trains_meta_data(z).sample_rate;
         sp_trains(i).sp = spike_trains_meta_data(z).sp;
-        [~, sp_trains(i).data] = epochRawData(spike_trains_meta_data(z).cell_id, spike_trains_meta_data(z).epoch_number, spike_trains_meta_data(z).channel);     
+        [~, sp_trains(i).data] = epochRawData(spike_trains_meta_data(z).cell_id, spike_trains_meta_data(z).epoch_number, spike_trains_meta_data(z).channel);
+        z=z+1;
         if z > N_trains
             break;
         end
@@ -26,6 +27,6 @@ while z < N_trains
     fname = sprintf('%sspike_trains_part_%02d', storage_location, p)
     fprintf('Time elapsed: %d\n', toc);
     save(fname, 'sp_trains','-v7.3')
-    z=z+trains_per_file;
+    %z=z+trains_per_file;
     p=p+1;
 end
