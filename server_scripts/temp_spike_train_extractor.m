d3d0639d3920af9daa778cd55bb806badb22a991
Greg Schwartz <greg.schwartz@northwestern.edu>
2022-11-26 13:52:52 -0600

    Update temp_spike_train_extractor.m

--- server_scripts/temp_spike_train_extractor.m
+++ server_scripts/temp_spike_train_extractor.m
@@ -12,18 +12,20 @@ while z < N_trains
     tic;
     sp_trains = repmat(struct,trains_per_file,1);
     for i=1:trains_per_file
+        i
         sp_trains(i).dataset_index = z;
         sp_trains(i).cell_unid = spike_trains_meta_data(z).cell_unid;
         sp_trains(i).cell_type = spike_trains_meta_data(z).cell_type;
+        sp_trains(i).sample_rate = spike_trains_meta_data(z).sample_rate;
         sp_trains(i).sp = spike_trains_meta_data(z).sp;
         [~, sp_trains(i).data] = epochRawData(spike_trains_meta_data(z).cell_id, spike_trains_meta_data(z).epoch_number, spike_trains_meta_data(z).channel);     
         if z > N_trains
             break;
         end
     end
-    fname = sprintf('%sspike_trains_part_%02d', storage_location, p);
-    toc;
-    save(fname, 'sp_trains','-v7.3');
+    fname = sprintf('%sspike_trains_part_%02d', storage_location, p)
+    fprintf('Time elapsed: %d\n', toc);
+    save(fname, 'sp_trains','-v7.3')
     z=z+trains_per_file;
     p=p+1;
 end
