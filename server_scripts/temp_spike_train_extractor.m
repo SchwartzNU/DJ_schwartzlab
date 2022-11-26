7ed58fd4da0d20c2cd9cb04a63308d553f7bb0dd
Greg Schwartz <greg.schwartz@northwestern.edu>
2022-11-26 12:53:26 -0600

    Create temp_spike_train_extractor.m

---
+++ server_scripts/temp_spike_train_extractor.m
@@ -0,0 +1,27 @@
+sl_epochs = sl_mutable.SpikeTrain * sl.SymphonyRecordedCell * sl_mutable.CurrentCellType * sl.Epoch & 'recording_mode = "Cell attached"';
+spike_trains_meta_data = fetch(sl_epochs,'*');
+
+N_trains = length(spike_trains_meta_data)
+trains_per_file = 5000
+
+z=1;
+p = 1;
+while z < N_trains
+    tic;
+    sp_trains = repmat(struct,trains_per_file,1);
+    for i=1:trains_per_file
+        sp_trains(i).dataset_index = z;
+        sp_trains(i).cell_unid = spike_trains_meta_data(z).cell_unid;
+        sp_trains(i).cell_type = spike_trains_meta_data(z).cell_type;
+        sp_trains(i).sp = spike_trains_meta_data(z).sp;
+        [~, sp_trains(i).data] = epochRawData(spike_trains_meta_data(z).cell_id, spike_trains_meta_data(z).epoch_number, spike_trains_meta_data(z).channel);     
+        if z > N_trains
+            break;
+        end
+    end
+    fname = sprintf('spike_trains_part_%02d', p)
+    toc;
+    save(fname, 'sp_trains','-v7.3');
+    z=z+trains_per_file;
+    p=p+1;
+end
