7c2ead3ce189ccd72ee3f6af0699d0e9acbacf98
Greg Schwartz <greg.schwartz@northwestern.edu>
2022-11-27 19:23:31 -0600

    Update copy_data_to_master.m

--- behavior_analysis/copy_data_to_master.m
+++ behavior_analysis/copy_data_to_master.m
@@ -5,8 +5,9 @@ N = height(found_sessions_copy);
 for i=1:N
     i
     new_folder_name = folder_name_from_behavior_session(found_sessions_copy.event_id(i));
-    destination = ['''' root_dir new_folder_name filesep ''''];
-    contents = dir(destination);
+    destination = ['''' master_dir new_folder_name filesep ''''];
+    dest = [master_dir new_folder_name filesep];
+    contents = dir(dest)
     if length(contents)==2 %empty
         source = ['''' behavior_dir found_sessions_copy.folder_name{i} filesep ''''];
         command = sprintf('cp -r %s* %s', source, destination);
