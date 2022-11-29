d28b55617a6710efaa446af734d387bab1b46c9a
Greg Schwartz <greg.schwartz@northwestern.edu>
2022-11-29 11:16:01 -0600

    Update find_duplicate_sessions.m
    wrote a piece to get rid of orphan folders (ones without sessions)

--- behavior_analysis/find_duplicate_sessions.m
+++ behavior_analysis/find_duplicate_sessions.m
@@ -27,3 +27,36 @@ for i=1:N
         pause;
     end
 end
+
+%% remove orphan folders from BehaviorMaster
+master_dir = '/Volumes/SchwartzLab/BehaviorMaster/';
+all_sessions = sln_animal.AnimalEvent * sln_animal.SocialBehaviorSession;
+D = dir(master_dir);
+for i=1:length(D)
+    cur_animal = D(i).name;
+    if ~startsWith(cur_animal, '.')
+       D_inner = dir([master_dir cur_animal]);
+       for j=1:length(D_inner)
+           folder_name = D_inner(j).name;
+           if ~startsWith(folder_name, '.')
+                id = str2double(extractBefore(folder_name,'_'));
+                q = all_sessions & sprintf('event_id=%d', id);
+                if ~q.exists
+                    fname = [master_dir cur_animal filesep folder_name];
+                    fprintf('Removing directory %s\n', fname);
+                    %answer = input(sprintf('Remove directory %s? [y|n]', fname), 's');
+%                    if strcmp(answer, 'y')
+                        rmdir(fname, 's');                        
+                        %pause;
+%                    end
+                end
+           end
+       end
+    end
+end
+
+
+
+
+
+
