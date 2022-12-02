5b624a2c7cf404991776de4552ebe696676f31bb
Greg Schwartz <greg.schwartz@northwestern.edu>
2022-12-01 22:23:43 -0600

    behavior session annotation imports

---
+++ schemas/+sl_behavior/duration_for_events_in_session.m
@@ -0,0 +1,33 @@
+function [d, frac] = duration_for_events_in_session(event_id, annotation_type, modifier)
+if nargin<3
+    modifier = [];
+end
+
+frame_rate = 15; %frames per second: TODO read this in from some preference file
+
+if isempty(modifier)
+    q = sl_behavior.Annotation & ...
+        sprintf('event_id=%d', event_id) & ...
+        sprintf('annotation_name="%s"', annotation_type);
+else
+    q = sl_behavior.Annotation & ...
+        sprintf('event_id=%d', event_id) & ...
+        sprintf('annotation_name="%s"', annotation_type) & ...
+        sprintf('modifier="%s"', modifier);
+end
+
+d = fetch1(aggr(sln_animal.SocialBehaviorSession & sprintf('event_id=%d', event_id), ...
+    q, 'sum(duration)->dur'), 'dur');
+
+start_frame = fetch1(sl_behavior.Annotation & ...
+        sprintf('event_id=%d', event_id) & ...
+        'annotation_name="door open"', 'frame');
+
+last_frame = fetch1(aggr(sln_animal.SocialBehaviorSession & sprintf('event_id=%d', event_id), ...
+    sl_behavior.Annotation & sprintf('event_id=%d', event_id), ...
+    'max(frame)->max_frame'), 'max_frame');
+
+total_time = double(last_frame - start_frame) / frame_rate;
+
+d = d / frame_rate;
+frac = d / total_time;
