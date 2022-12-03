b70cfe242d345d1ade58eaf90b90b2a84850208d
Zach Jessen <z.jessen@gmail.com>
2022-12-02 23:55:50 -0600

    new SMS

---
+++ schemas/+sln_symphony/ExperimentProtocolSpotsMultiSizeV2BlockParameters.m
@@ -0,0 +1,37 @@
+%{
+#Block parameters for SpotsMultiSize (2) 
+-> sln_symphony.ExperimentEpochBlock
+---
+do_subtraction = NULL : float
+imaging_field_height : float
+imaging_field_width : float
+imaging_mean : float
+intensity : float
+log_scaling : enum('F','T') #bool
+max_size : float
+mean_level : float
+min_size : float
+number_of_cycles : smallint unsigned
+number_of_size_steps : smallint unsigned
+pre_time : float
+random_ordering : enum('F','T') #bool
+rstar_mean : float
+rstar_midground : float
+stim_time : float
+tail_time : float
+%}
+classdef ExperimentProtocolSpotsMultiSizeV2BlockParameters < sln_symphony.ExperimentProtocol
+	properties
+
+		%attributes to be renamed
+		renamed_attributes = struct();
+
+		%attributes to be removed from the key
+		dropped_attributes = {};
+	end
+	methods
+		function block_key = add_attributes(self, block_key, epoch_key) %#ok<INUSL,INUSD>
+		%add entities to the key based on others
+		end
+	end
+end
