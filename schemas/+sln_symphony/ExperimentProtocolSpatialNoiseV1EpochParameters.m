193b8f53c49ec431525b98cfc48c9f67d4631322
Zach Jessen <z.jessen@gmail.com>
2022-12-03 12:15:00 -0600

    some updates to params

---
+++ schemas/+sln_symphony/ExperimentProtocolSpatialNoiseV1EpochParameters.m
@@ -0,0 +1,23 @@
+%{
+#Epoch parameters for SpatialNoise (1) 
+-> sln_symphony.ExperimentEpoch
+---
+noise_seed : float
+offset_seed : float
+protocol_version : float
+%}
+classdef ExperimentProtocolSpatialNoiseV1EpochParameters < sln_symphony.ExperimentProtocol
+	properties
+
+		%attributes to be renamed
+		renamed_attributes = struct();
+
+		%attributes to be removed from the key
+		dropped_attributes = {};
+	end
+	methods
+		function epoch_key = add_attributes(self, block_key, epoch_key) %#ok<INUSL,INUSD>
+		%add entities to the key based on others
+		end
+	end
+end
