e9d7831d9a65eac4b84b1523bc64e98eb4170b9e
SchwartzLabNU <SchwartzLabNU@gmail.com>
2022-12-07 15:35:59 -0600

    behavior_analysis

--- behavior_analysis/copy_data_to_master.m
+++ behavior_analysis/copy_data_to_master.m
@@ -1,8 +1,9 @@
-master_dir = '/Volumes/SchwartzLab/BehaviorMaster/';
-behavior_dir = '/Volumes/SchwartzLab/Behavior/';
+master_dir = '\\fsmresfiles.fsm.northwestern.edu\fsmresfiles\Ophthalmology\Research\SchwartzLab\BehaviorMaster\';
+behavior_dir = '\\fsmresfiles.fsm.northwestern.edu\fsmresfiles\Ophthalmology\Research\SchwartzLab\Behavior\';
 N = height(found_sessions_copy);
 
-for i=1:N
+startPos = 768;
+for i=startPos:N
     i
     new_folder_name = folder_name_from_behavior_session(found_sessions_copy.event_id(i));
     destination = ['''' master_dir new_folder_name filesep ''''];
@@ -10,8 +11,9 @@ for i=1:N
     contents = dir(dest)
     if length(contents)==2 %empty
         source = ['''' behavior_dir found_sessions_copy.folder_name{i} filesep ''''];
-        command = sprintf('cp -r %s* %s', source, destination);
-        unix(command);
+        %command = sprintf('cp -r %s* %s', source, destination);
+        command = sprintf('xcopy %s %s', source, destination);
+        system(command);
     end
 end
 
