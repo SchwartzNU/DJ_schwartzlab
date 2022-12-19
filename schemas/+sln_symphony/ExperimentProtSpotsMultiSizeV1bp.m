5b1e22a8fe40e430bafb6f05a810b33edf6e0a7f
Greg Schwartz <greg.schwartz@northwestern.edu>
2022-12-19 12:09:34 -0600

    Update ExperimentProtSpotsMultiSizeV1bp.m

--- schemas/+sln_symphony/ExperimentProtSpotsMultiSizeV1bp.m
+++ schemas/+sln_symphony/ExperimentProtSpotsMultiSizeV1bp.m
@@ -16,25 +16,32 @@ stim_time                   : float                         #
 tail_time                   : float                         # 
 %}
 classdef ExperimentProtSpotsMultiSizeV1bp < sln_symphony.ExperimentProtocol
-	properties
+    properties
 
-		%attributes to be renamed
-		renamed_attributes = struct();
+        %attributes to be renamed
+        renamed_attributes = struct();
 
-		%attributes to be removed from the key
-		dropped_attributes = {};
-	end
-	methods
-		function block_key = add_attributes(self, block_key, epoch_key) %#ok<INUSL,INUSD>            
-		%add entities to the key based on others            
-        block_key
-        pause;
-%             if isfield(block_key,'logScaling')
-%                 if block_key.logScaling
-%                     
-%                 end
-% 
-%             end
-		end
-	end
+        %attributes to be removed from the key
+        dropped_attributes = {};
+    end
+    methods
+        function block_key = add_attributes(self, block_key, epoch_key) %#ok<INUSL,INUSD>
+            %add entities to the key based on others
+            if isfield(block_key,'log_scaling')
+                if block_key.log_scaling
+                    block_key.scaling = 'log';
+                else
+                    block_key.scaling = 'linear';
+                end
+                block_key = rmfield(block_key, 'log_scaling');
+            end
+
+            if isfield(block_key,'pick_specific_sizes')
+                if block_key.pick_specific_sizes
+                    block_key.scaling = 'custom';
+                end
+                block_key = rmfield(block_key, 'pick_specific_sizes');
+            end
+        end
+    end
 end
