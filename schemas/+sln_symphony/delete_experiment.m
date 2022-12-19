3a65d93d886971c06f05f1fe3e07d304f50b9ce2
Greg Schwartz <greg.schwartz@northwestern.edu>
2022-12-19 12:50:36 -0600

    updates for new protocols and trying to get delete_experiment working

---
+++ schemas/+sln_symphony/delete_experiment.m
@@ -0,0 +1,13 @@
+function [] = delete_experiment(exp_name)
+c = sln_symphony.Experiment().descendants;
+
+vc = cellfun(@(x) exist(x,'class'), c) == 8;
+c(~vc) = [];
+d = cellfun(@(x) length(feval(x).descendants), c);
+
+[~,i] = sort(d);
+
+
+for ci = i
+    delQuick(feval(c{ci}) & sprintf("`file_name` = '%s'", exp_name));
+end
