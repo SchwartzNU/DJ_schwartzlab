f64b6b9f4e86ecae4faff92ce8d669308c4db932
Greg Schwartz <greg.schwartz@northwestern.edu>
2022-12-20 14:01:08 -0600

    adding protocols

---
+++ schemas/+sln_symphony/ExperimentProtCenterSurroundNoiseV1ep.m
@@ -0,0 +1,24 @@
+%{
+#e parameters for CenterSurroundNoise (1) 
+-> sln_symphony.ExperimentEpoch
+---
+center_noise_seed : float
+current_stimulus : varchar(64)
+protocol_version : float
+surround_noise_seed : float
+%}
+classdef ExperimentProtCenterSurroundNoiseV1ep < sln_symphony.ExperimentProtocol
+	properties
+
+		%attributes to be renamed
+		renamed_attributes = struct();
+
+		%attributes to be removed from the key
+		dropped_attributes = {};
+	end
+	methods
+		function epoch_key = add_attributes(self, block_key, epoch_key) %#ok<INUSL,INUSD>
+		%add entities to the key based on others
+		end
+	end
+end
