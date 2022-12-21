ae823d4414746d00c3899bd5d99a9e577b4bdb6d
Greg Schwartz <greg.schwartz@northwestern.edu>
2022-12-21 16:54:25 -0600

    Update ExperimentProtRandomMotionEdgeV2bp.m

--- schemas/+sln_symphony/ExperimentProtRandomMotionEdgeV2bp.m
+++ schemas/+sln_symphony/ExperimentProtRandomMotionEdgeV2bp.m
@@ -17,7 +17,7 @@ pre_time : float
 rstar_mean : float
 stim_time : float
 tail_time : float
-single_edge_mode : enum('F','T')
+single_edge_mode : smallint unsigned
 single_edge_polarity : varchar(16)
 %}
 classdef ExperimentProtRandomMotionEdgeV2bp < sln_symphony.ExperimentProtocol
