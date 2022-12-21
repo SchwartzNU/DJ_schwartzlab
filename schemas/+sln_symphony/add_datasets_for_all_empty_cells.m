7150efeec31cd277f1a02f7d03313e8ad265e591
Greg Schwartz <greg.schwartz@northwestern.edu>
2022-12-21 10:50:44 -0600

    Update add_datasets_for_all_empty_cells.m

--- schemas/+sln_symphony/add_datasets_for_all_empty_cells.m
+++ schemas/+sln_symphony/add_datasets_for_all_empty_cells.m
@@ -1,6 +1,8 @@
 empty_cells = sln_symphony.ExperimentCell - sln_symphony.Dataset;
 experiments_with_empty_cells = unique(fetchn(empty_cells, 'file_name'));
 
+error_exps = {};
+
 N = length(experiments_with_empty_cells)
 for i=1:N
     fprintf('%d of %d', i, N);
@@ -8,6 +10,7 @@ for i=1:N
     try
         sln_symphony.insert_all_datasets_for_experiment(experiments_with_empty_cells{i});
     catch ME
+        error_exps = [error_exps; experiments_with_empty_cells{i}];
         disp(ME.message);
     end
 end
