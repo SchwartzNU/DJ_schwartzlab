dc9bd691503b2e104fd122ae3bb8219abc69b8a4
Greg Schwartz <greg.schwartz@northwestern.edu>
2022-12-21 17:06:14 -0600

    Update ExperimentProtRandomMotionEdgeV2bp.m

--- schemas/+sln_symphony/ExperimentProtRandomMotionEdgeV2bp.m
+++ schemas/+sln_symphony/ExperimentProtRandomMotionEdgeV2bp.m
@@ -1,24 +1,24 @@
 %{
-#b parameters for RandomMotionEdge (1) 
+# b parameters for RandomMotionEdge (2)
 -> sln_symphony.ExperimentEpochBlock
 ---
-angle_offset : float
-bar_length : float
-bar_width : float
-intensity : float
-mean_level : float
-motion_lowpass_filter_passband : float
-motion_lowpass_filter_stopband : float
-motion_seed : float
-motion_standard_deviation : float
-number_of_angles : smallint unsigned
-number_of_cycles : smallint unsigned
-pre_time : float
-rstar_mean : float
-stim_time : float
-tail_time : float
-single_edge_mode : smallint unsigned
-single_edge_polarity : smallint unsigned
+angle_offset                : float                         # 
+bar_length                  : float                         # 
+bar_width                   : float                         # 
+intensity                   : float                         # 
+mean_level                  : float                         # 
+motion_lowpass_filter_passband: float                       # 
+motion_lowpass_filter_stopband: float                       # 
+motion_seed                 : float                         # 
+motion_standard_deviation   : float                         # 
+number_of_angles            : smallint unsigned             # 
+number_of_cycles            : smallint unsigned             # 
+pre_time                    : float                         # 
+rstar_mean                  : float                         # 
+stim_time                   : float                         # 
+tail_time                   : float                         # 
+single_edge_mode            : smallint unsigned             # 
+single_edge_polarity        : smallint unsigned             # 
 %}
 classdef ExperimentProtRandomMotionEdgeV2bp < sln_symphony.ExperimentProtocol
 	properties
