cc1a11621ce662daa443b07f3e595c3fcf3f1df1
Greg Schwartz <greg.schwartz@northwestern.edu>
2022-12-21 22:41:48 -0600

    prot updates

---
+++ schemas/+sln_symphony/ExperimentProtSpotsMultiSizeV4ep.m
@@ -0,0 +1,22 @@
+%{
+#Epoch parameters for SpotsMultiSize (4) 
+-> sln_symphony.ExperimentEpoch
+---
+cur_spot_size : float
+protocol_version : float
+%}
+classdef ExperimentProtSpotsMultiSizeV4ep < sln_symphony.ExperimentProtocol
+	properties
+
+		%attributes to be renamed
+		renamed_attributes = struct();
+
+		%attributes to be removed from the key
+		dropped_attributes = {};
+	end
+	methods
+		function epoch_key = add_attributes(self, block_key, epoch_key) %#ok<INUSL,INUSD>
+		%add entities to the key based on others
+		end
+	end
+end
