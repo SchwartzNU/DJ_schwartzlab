6bd9fc017f0bf4e68ad75dd93b94a0227647c8f0
Greg Schwartz <greg.schwartz@northwestern.edu>
2022-12-22 16:33:41 -0600

    add

---
+++ schemas/+sln_symphony/ExperimentProtColorIsoResponseV3bp.m
@@ -0,0 +1,50 @@
+%{
+# Block parameters for ColorIsoResponse (3)
+-> sln_symphony.ExperimentEpochBlock
+---
+annulus_mode                : enum('F','T')                 # 
+pre_time                    : float                         # 
+rstar_mean                  : float                         # 
+spot_diameter               : float                         # 
+stim_time                   : float                         # 
+tail_time                   : float                         # 
+background_pattern          : tinyint unsigned              # 
+base_intensity_1            : float                         # 
+base_intensity_2            : float                         # 
+enable_surround             : tinyint unsigned              # 
+primary_object_pattern      : tinyint unsigned              # 
+secondary_object_pattern    : tinyint unsigned              # 
+surround_diameter           : float                         # 
+mstar_intensity             : float                         # 
+sstar_intensity             : float                         # 
+mean_level                  : float
+%}
+classdef ExperimentProtColorIsoResponseV3bp < sln_symphony.ExperimentProtocol
+    properties
+
+        %attributes to be renamed
+        renamed_attributes = struct();
+
+        %attributes to be removed from the key
+        dropped_attributes = {'session_id', 'color_combination_mode'};
+    end
+    methods
+        function block_key_new = add_attributes(self, block_key, epoch_key) %#ok<INUSL,INUSD>
+            %add entities to the key based on others
+
+            for i=1:length(block_key)
+                b = block_key(i);
+                if isfield(b,'annulus_mode')
+                    if b.annulus_mode
+                        b.annulus_mode = 'T';
+                    else
+                        b.annulus_mode = 'F';
+                    end
+                else
+                    b.annulus_mode = 'F';
+                end
+                block_key_new(i) = b;
+            end
+        end
+    end
+end
