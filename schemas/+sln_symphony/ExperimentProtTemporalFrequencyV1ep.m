42f85a48091806b870c490f12a9e93e80bde73a3
davidswygart <davidswygart2015@u.northwestern.edu>
2022-12-22 15:07:03 -0600

    adding prot

---
+++ schemas/+sln_symphony/ExperimentProtTemporalFrequencyV1ep.m
@@ -0,0 +1,22 @@
+%{
+#e parameters for TemporalFrequency (1) 
+-> sln_symphony.ExperimentEpoch
+---
+cur_frequency : float
+protocol_version : float
+%}
+classdef ExperimentProtTemporalFrequencyV1ep < sln_symphony.ExperimentProtocol
+	properties
+
+		%attributes to be renamed
+		renamed_attributes = struct();
+
+		%attributes to be removed from the key
+		dropped_attributes = {};
+	end
+	methods
+		function epoch_key = add_attributes(self, block_key, epoch_key) %#ok<INUSL,INUSD>
+		%add entities to the key based on others
+		end
+	end
+end
