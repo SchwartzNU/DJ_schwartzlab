9a043c709cc91d365c6e0514961d4ec93c894b10
Greg Schwartz <greg.schwartz@northwestern.edu>
2022-12-23 13:20:32 -0600

    add

---
+++ schemas/+sln_symphony/ExperimentProtChirpV4ep.m
@@ -0,0 +1,21 @@
+%{
+#Epoch parameters for Chirp (4) 
+-> sln_symphony.ExperimentEpoch
+---
+protocol_version : float
+%}
+classdef ExperimentProtChirpV4ep < sln_symphony.ExperimentProtocol
+	properties
+
+		%attributes to be renamed
+		renamed_attributes = struct();
+
+		%attributes to be removed from the key
+		dropped_attributes = {};
+	end
+	methods
+		function epoch_key = add_attributes(self, block_key, epoch_key) %#ok<INUSL,INUSD>
+		%add entities to the key based on others
+		end
+	end
+end
