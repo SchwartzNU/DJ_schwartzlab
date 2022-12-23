3a4f2d18edc99737d23d565395cbf4dfe0a02b36
Greg Schwartz <greg.schwartz@northwestern.edu>
2022-12-23 14:08:49 -0600

    add

---
+++ schemas/+sln_symphony/ExperimentProtSpatialNoiseV5ep.m
@@ -0,0 +1,23 @@
+%{
+#Epoch parameters for SpatialNoise (5) 
+-> sln_symphony.ExperimentEpoch
+---
+offset_seed : float
+noise_seed : float
+protocol_version : float
+%}
+classdef ExperimentProtSpatialNoiseV5ep < sln_symphony.ExperimentProtocol
+	properties
+
+		%attributes to be renamed
+		renamed_attributes = struct();
+
+		%attributes to be removed from the key
+		dropped_attributes = {};
+	end
+	methods
+		function epoch_key = add_attributes(self, block_key, epoch_key) %#ok<INUSL,INUSD>
+		%add entities to the key based on others
+		end
+	end
+end
