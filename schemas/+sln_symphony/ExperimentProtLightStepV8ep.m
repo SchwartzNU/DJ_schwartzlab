e16ec13282426c969fe6187d65127f54c8e811ad
Greg Schwartz <greg.schwartz@northwestern.edu>
2022-12-22 18:36:34 -0600

    updates

---
+++ schemas/+sln_symphony/ExperimentProtLightStepV8ep.m
@@ -0,0 +1,21 @@
+%{
+#Epoch parameters for LightStep (8) 
+-> sln_symphony.ExperimentEpoch
+---
+protocol_version : float
+%}
+classdef ExperimentProtLightStepV8ep < sln_symphony.ExperimentProtocol
+	properties
+
+		%attributes to be renamed
+		renamed_attributes = struct();
+
+		%attributes to be removed from the key
+		dropped_attributes = {'current_spot_pattern'};
+	end
+	methods
+		function epoch_key = add_attributes(self, block_key, epoch_key) %#ok<INUSL,INUSD>
+		%add entities to the key based on others
+		end
+	end
+end
