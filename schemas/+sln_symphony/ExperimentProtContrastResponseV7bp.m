6824ed87b160f5825e5a6e4c825567b093c84406
Greg Schwartz <greg.schwartz@northwestern.edu>
2022-12-22 18:27:26 -0600

    add

---
+++ schemas/+sln_symphony/ExperimentProtContrastResponseV7bp.m
@@ -0,0 +1,38 @@
+%{
+#Block parameters for ContrastResponse (7) 
+-> sln_symphony.ExperimentEpochBlock
+---
+contrast_direction : enum('negative','positive','both')
+max_contrast : float
+mean_level : float
+min_contrast : float
+number_of_contrast_steps : smallint unsigned
+number_of_cycles : smallint unsigned
+pre_time : float
+real_number_of_contrast_steps : smallint unsigned
+spot_diameter : float
+stim_time : float
+tail_time : float
+rstar_mean = NULL : float
+background_pattern : smallint unsigned
+primary_object_pattern : smallint unsigned
+secondary_object_pattern : smallint unsigned
+mstar_intensity : float
+sstar_intensity : float
+color_combination_mode : varchar(32)
+%}
+classdef ExperimentProtContrastResponseV7bp < sln_symphony.ExperimentProtocol
+	properties
+
+		%attributes to be renamed
+		renamed_attributes = struct();
+
+		%attributes to be removed from the key
+		dropped_attributes = {'red_led'};
+	end
+	methods
+		function block_key = add_attributes(self, block_key, epoch_key) %#ok<INUSL,INUSD>
+		%add entities to the key based on others
+		end
+	end
+end
