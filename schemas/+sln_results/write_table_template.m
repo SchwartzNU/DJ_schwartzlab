a6f123ceb21d1343c49f2777020e71899c0716e3
Greg Schwartz <greg.schwartz@northwestern.edu>
2022-12-25 16:15:51 -0600

    updates for excel table stuff

--- schemas/+sln_results/write_table_template.m
+++ schemas/+sln_results/write_table_template.m
@@ -10,8 +10,14 @@ T = table('size',[N_vars 3],...
 T.Field = R.Properties.VariableNames';
 T.Description = R.Properties.VariableDescriptions';
 for i=1:length(R.Properties.VariableNames)
-    T.Type(i) = class(R.(R.Properties.VariableNames{i}));
-    if strcmp(R.Properties.VariableNames{i}, 'git_tag') || strcmp(R.Properties.VariableNames{i}, 'user_name')
+    cur_var = R.Properties.VariableNames{i};
+    cur_class = class(R.(cur_var));
+    if strcmp(cur_class,'char')
+        T.Type(i) = 'string';
+    else
+         T.Type(i) = cur_class;
+    end
+    if strcmp(cur_var, 'git_tag') || strcmp(cur_var, 'user_name') || strcmp(cur_var, 'entry_time')
         keep_rows(i) = false;
     end
 end
