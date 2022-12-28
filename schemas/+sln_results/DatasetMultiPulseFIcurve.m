7a74b58dd5d065c65322fe921feca257390185ec
Greg Schwartz <greg.schwartz@northwestern.edu>
2022-12-28 13:02:05 -0600

    bug fixes

--- schemas/+sln_results/DatasetMultiPulseFIcurve.m
+++ schemas/+sln_results/DatasetMultiPulseFIcurve.m
@@ -5,12 +5,13 @@ dataset_name : varchar(128) # dataset name
 source_id : int unsigned # source id used to identify the cell to which the dataset belongs
 ---
 -> sln_lab.User # user who entered this result
+entry_time = CURRENT_TIMESTAMP : timestamp # time the result was entered
 fr_per_current_mean=NULL : longblob # mean firing rate (Hz) for each current injection
 fr_per_current_sem=NULL : longblob # sem of firing rate (Hz) for each current injection
-entry_time = CURRENT_TIMESTAMP : timestamp # time the result was entered
 git_tag : varchar(128) # git tag of current version of DJ_ROOT folder
 inj_current=NULL : longblob # vector of current injection amplitudes (pA)
 n_epochs_per_current=NULL : longblob # vector with how many trials for each injection size
+stim_time_s=NULL : float # stimulus time (s)
 %}
 classdef DatasetMultiPulseFIcurve < dj.Manual
 end
