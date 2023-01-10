c31b6449c5962f6dae13e4143b4d31f07bd3f203
Greg Schwartz <greg.schwartz@northwestern.edu>
2023-01-09 20:34:59 -0600

    Create UserParamCellBadRecording.m

---
+++ schemas/+sln_symphony/UserParamCellBadRecording.m
@@ -0,0 +1,8 @@
+%{
+# UserParamCellBadRecording
+-> sln_symphony.ExperimentCell
+---
+bad_recording = NULL : enum('T','F') # bad recording for some reason
+%}
+classdef UserParamCellBadRecording < dj.Manual
+end
