e9220ff4accf38912f016ff7864a9d90048ce572
Greg Schwartz <greg.schwartz@northwestern.edu>
2023-01-19 18:18:24 -0600

    Update double_exp_decay.m

--- nlinfit_curve_fitters/double_exp_decay.m
+++ nlinfit_curve_fitters/double_exp_decay.m
@@ -8,6 +8,6 @@ c1 = params(3);
 %compute y_hat from your equation
 y_hat = c1*exp(-tau1 * x) + (1-c1)*exp(-tau2 * x);
 
-% %don't allow nan or inf values. nlinfit does not like it!
-% y_hat(isnan(y_hat)) = 0;
-% y_hat(isinf(y_hat)) = 0;
+%don't allow nan or inf values. nlinfit does not like it!
+y_hat(isnan(y_hat)) = 0;
+y_hat(isinf(y_hat)) = 0;
