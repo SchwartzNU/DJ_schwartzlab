f2ca670ccdbf7e457d2e3eb4e0a4a8f58aa99344
devongreer <devongreer2022@u.northwestern.edu>
2023-03-01 12:54:00 -0600

    Update queryingSLBehaviorSessions.m

--- behavior_analysis/queryingSLBehaviorSessions.m
+++ behavior_analysis/queryingSLBehaviorSessions.m
@@ -1,4 +1,8 @@
 load behaviorParams
 NonvirginLargeDS_cagemates =fetch(sl.AnimalEventSocialBehaviorSession & 'date<="2021-10-01"'&'date>="2021-08-01"'& 'purpose="cagemate_2_strangers"')
-results_NonvirginLargeDS_cagemates = collectDataAcrossSessions(NonvirginLargeDS_cagemates, P, {'Female', 'Male', 'Male'})
+results_NonvirginLargeDS_cagemates = collectDataAcrossSessions(NonvirginLargeDS_cagemates, P,  {'cagemate', 'novel object', 'novel object'})
 exportBehvaiorDataForIgor(results_NonvirginLargeDS_cagemates, 'body_s', 'results_NonvirginLargeDS_1f2m_BodySeconds2')
+
+NonvirginLargeDS_1f2m =fetch(sl.AnimalEventSocialBehaviorSession & 'date<="2021-10-01"'&'date>="2021-08-01"'& 'purpose="cagemate_2_strangers"')
+results_NonvirginLargeDS_1f2m = collectDataAcrossSessions(NonvirginLargeDS_cagemates, P, {'female', 'male', 'male'})
+exportBehvaiorDataForIgor(results_NonvirginLargeDS_1f2m, 'body_s', 'results_NonvirginLargeDS_1f2m_BodySeconds2')
