82cb5036b657065ed161efb5c72486fca6612dd1
Greg Schwartz <greg.schwartz@northwestern.edu>
2023-03-07 11:57:56 -0600

    add contrast response analysis

--- DJ_analysis_Greg/ContrastResponse_CA.m
+++ DJ_analysis_Greg/ContrastResponse_CA.m
@@ -37,39 +37,39 @@ for d=1:N_datasets
     total_ms = pre_stim_tail.pre_time + pre_stim_tail.stim_time + pre_stim_tail.tail_time;
     psth_length = ceil(total_ms/binSize);
 
-    all_spot_sizes = round([epochs_in_dataset.cur_spot_size]);
-    spot_sizes = sort(unique(all_spot_sizes));
-    N_spot_sizes = length(spot_sizes);
+    all_contrasts = round([epochs_in_dataset.contrast]);
+    contrasts = sort(unique(all_contrasts));
+    N_contrasts = length(contrasts);
 
-    N_epochs_per_size = zeros(N_spot_sizes,1);
-    spikes_pre_mean = zeros(N_spot_sizes,1);
-    spikes_stim_mean = zeros(N_spot_sizes,1);
-    spikes_tail_mean = zeros(N_spot_sizes,1);
-    spikes_stim_sem = zeros(N_spot_sizes,1);
-    spikes_tail_sem = zeros(N_spot_sizes,1);
-    sms_psth = zeros(N_spot_sizes,psth_length);
+    N_epochs_per_contrast = zeros(N_contrasts,1);
+    spikes_pre_mean = zeros(N_contrasts,1);
+    spikes_stim_mean = zeros(N_contrasts,1);
+    spikes_tail_mean = zeros(N_contrasts,1);
+    spikes_stim_sem = zeros(N_contrasts,1);
+    spikes_tail_sem = zeros(N_contrasts,1);
+    psth_by_contrast = zeros(N_contrasts,psth_length);
 
-    for s=1:N_spot_sizes
-        ind = find(all_spot_sizes == spot_sizes(s));
-        N_epochs_per_size(s) = length(ind);
-        pre_spikes = zeros(N_epochs_per_size(s),1);
-        stim_spikes = zeros(N_epochs_per_size(s),1);
-        tail_spikes = zeros(N_epochs_per_size(s),1);
-        for i=1:N_epochs_per_size(s)
+    for s=1:N_contrasts
+        ind = find(all_contrasts == contrasts(s));
+        N_epochs_per_contrast(s) = length(ind);
+        pre_spikes = zeros(N_epochs_per_contrast(s),1);
+        stim_spikes = zeros(N_epochs_per_contrast(s),1);
+        tail_spikes = zeros(N_epochs_per_contrast(s),1);
+        for i=1:N_epochs_per_contrast(s)
             pre_spikes(i) = spikes_in_interval(epochs_in_dataset(ind(i)),pre_stim_tail,'pre');
             stim_spikes(i) = spikes_in_interval(epochs_in_dataset(ind(i)),pre_stim_tail,'stim');
             tail_spikes(i) = spikes_in_interval(epochs_in_dataset(ind(i)),pre_stim_tail,'tail');
         end
         if s==1
-            [psth_x, sms_psth(s,:)] = psth_for_epochs(epochs_in_dataset_table & epochs_in_dataset(ind), binSize);
+            [psth_x, psth_by_contrast(s,:)] = psth_for_epochs(epochs_in_dataset_table & epochs_in_dataset(ind), binSize);
         else
-            [~, sms_psth(s,:)] = psth_for_epochs(epochs_in_dataset_table & epochs_in_dataset(ind), binSize);
+            [~, psth_by_contrast(s,:)] = psth_for_epochs(epochs_in_dataset_table & epochs_in_dataset(ind), binSize);
         end
         spikes_pre_mean(s) = mean(pre_spikes);
         spikes_stim_mean(s) = mean(stim_spikes);
         spikes_tail_mean(s) = mean(tail_spikes);
-        spikes_stim_sem(s) = std(stim_spikes)./sqrt(N_epochs_per_size(s)-1);
-        spikes_tail_sem(s) = std(tail_spikes)./sqrt(N_epochs_per_size(s)-1);
+        spikes_stim_sem(s) = std(stim_spikes)./sqrt(N_epochs_per_contrast(s)-1);
+        spikes_tail_sem(s) = std(tail_spikes)./sqrt(N_epochs_per_contrast(s)-1);
     end
 
     baseline_rate = mean(spikes_pre_mean) / (pre_stim_tail.pre_time / 1E3); %baseline rate in Hz
@@ -78,17 +78,17 @@ for d=1:N_datasets
     R.file_name{d} = datasets_struct(d).file_name;
     R.dataset_name(d) = datasets_struct(d).dataset_name;
     R.source_id(d) = datasets_struct(d).source_id;
-    R.spot_sizes{d} = spot_sizes';
+    R.contrasts{d} = contrasts';
     R.pre_time_ms(d) = pre_stim_tail.pre_time;
     R.stim_time_ms(d) = pre_stim_tail.stim_time;
     R.tail_time_ms(d) = pre_stim_tail.tail_time;
-    R.n_epochs_per_size{d} = N_epochs_per_size;
+    R.n_epochs_per_contrast{d} = N_epochs_per_contrast;
     R.spikes_pre_mean{d} = spikes_pre_mean;
     R.spikes_stim_mean{d} = spikes_stim_mean;
     R.spikes_tail_mean{d} = spikes_tail_mean;
     R.spikes_stim_sem{d} = spikes_stim_sem;
     R.spikes_tail_sem{d} = spikes_tail_sem;
-    R.sms_psth{d} = sms_psth;
+    R.psth_by_contrast{d} = psth_by_contrast;
     R.psth_x{d} = psth_x;
     R.baseline_rate_hz(d) = baseline_rate;
     fprintf('Elapsed time = %d seconds\n', round(toc));
