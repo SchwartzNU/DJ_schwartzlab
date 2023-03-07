6631b8a22b84eae2f2793808213789c1fd8e6a0b
Greg Schwartz <greg.schwartz@northwestern.edu>
2023-03-07 12:46:20 -0600

    Update plot_ContrastResponse_CA.m

--- DJ_plotters_Greg/plot_ContrastResponse_CA.m
+++ DJ_plotters_Greg/plot_ContrastResponse_CA.m
@@ -9,7 +9,12 @@ set(ax, 'XLim',[-1 1]);
 errorbar(ax, R.contrasts, R.spikes_stim_mean, R.spikes_stim_sem,...
     'Color',[0 0 0],...
     'LineWidth',2);
+baseline_rate = interp1(R.contrasts, R.spikes_stim_mean, 0);
+hold(ax,'on');
+line(ax, [-1, 1], [baseline_rate, baseline_rate], 'LineStyle', '--', 'Color', 'k');
+line(ax, [0 0], [min(R.spikes_stim_mean), max(R.spikes_stim_mean)], 'LineStyle', '--', 'Color', 'k');
 set(ax,'XtickMode','auto');
 set(ax,'YtickMode','auto');
 xlabel(ax, 'Contrast')
 ylabel(ax, 'Spike count');
+hold(ax,'off');
