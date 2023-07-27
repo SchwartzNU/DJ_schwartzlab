bfeeb9db7e8242b7da7845a6cc9a0678da1b2ac9
Greg Schwartz <greg.schwartz@northwestern.edu>
2023-07-27 14:51:05 -0500

    added FI curve runner and deleted some old stuff

--- schemas/+sl_devon/getSchema.m
+++
@@ -1,7 +0,0 @@
-function obj = getSchema
-persistent schemaObject
-if isempty(schemaObject)
-    schemaObject = dj.Schema(dj.conn, 'sl_devon', 'sl_devon');
-end
-obj = schemaObject;
-end
--- schemas/+sl_kristine/getSchema.m
+++
@@ -1,7 +0,0 @@
-function obj = getSchema
-persistent schemaObject
-if isempty(schemaObject)
-    schemaObject = dj.Schema(dj.conn, 'sl_kristine', 'sl_kristine');
-end
-obj = schemaObject;
-end
