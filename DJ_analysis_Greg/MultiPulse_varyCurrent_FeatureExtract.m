04cba9f9a28c9355fe1bd62842d34c09affc1629
agnsinha99 <agnivasinha2023@u.northwestern.edu>
2023-08-04 17:00:20 -0500

    Update MultiPulse_varyCurrent_FeatureExtract.m

--- DJ_analysis_Greg/MultiPulse_varyCurrent_FeatureExtract.m
+++ DJ_analysis_Greg/MultiPulse_varyCurrent_FeatureExtract.m
@@ -180,7 +180,7 @@ for d=1:N_datasets
             % c ~ 1/(average capacticance for a neuron * resting_vm(trial)
             % 2) needs bounds
             dt = time_in_s(2)-time_in_s(1);
-            endInd = start_time+round(0.004/dt);
+            endInd = start_time+round(0.02/dt);
             xval = time_in_s(start_time:endInd)';
             xval = xval - xval(1);
             yval = hyper_Vm(start_time:endInd, hyper_epoch_less_than_minus50(i));
@@ -189,7 +189,7 @@ for d=1:N_datasets
             %plot(time_in_s,hyper_Vm(:,hyper_epoch_less_than_minus50(i)))%
             [f,gof] = fit(xval,yval , ft, 'StartPoint',[-60,10,30]);
             tau_array(i) =  f.c;
-             
+            
         end
         
         
@@ -197,7 +197,6 @@ for d=1:N_datasets
         
         B = rmoutliers(tau_array);
         tau_array_ms(trial) = mean(1./B)*1000;
-        
            
 
         %Return Capacitance
