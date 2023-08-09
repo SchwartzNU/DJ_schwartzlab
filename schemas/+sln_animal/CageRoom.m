6077907788669126d5cc679247cce9136a547c7b
SchwartzLabNU <SchwartzLabNU@gmail.com>
2023-08-09 12:28:08 -0500

    update cageroom

--- schemas/+sln_animal/CageRoom.m
+++ schemas/+sln_animal/CageRoom.m
@@ -1,10 +1,9 @@
 %{
-# cage room 
-room_number: varchar(16)      # room number
----
+# cage room
+room_number                 : varchar(16)                   # room number
 %}
 classdef CageRoom < dj.Lookup
     properties
-        contents = {'SB-237', 'SB-419', 'SB-443', 'SB-455','QB-802'}';
+        contents = {'In Lab', 'SB-237', 'SB-419', 'SB-443', 'SB-455','QB-802', 'W-188}';
     end
 end
