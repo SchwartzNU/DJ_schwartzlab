5efe0ddbf52904ce8e8e70c414268d941e68810d
Trung Tran-Le <38426554+trungtranle@users.noreply.github.com>
2023-09-27 10:56:11 -0500

    Update ExperimentElectrode.m

--- schemas/+sln_symphony/ExperimentElectrode.m
+++ schemas/+sln_symphony/ExperimentElectrode.m
@@ -2,10 +2,10 @@
 # A symphony electrode channel for a block of epochs
 -> sln_symphony.ExperimentChannel
 ---
-(cell_id) -> sln_symphony.ExperimentCell(source_id)
-recording_mode : enum('Voltage clamp','Current clamp') # units for this recording
-hold : float  # the hold signal in pA or mV
-amp_mode : enum('Cell attached','Whole cell')
+ (cell_id) -> sln_symphony.ExperimentCell
+recording_mode              : enum('Voltage clamp','Current clamp') # units for this recording
+hold                        : float                         # the hold signal in pA or mV
+amp_mode                    : enum('Cell attached','Whole cell','PerforatedEscin','PerforatedAmpho','PerforatedGrami') # 
 %}
 classdef ExperimentElectrode < sln_symphony.ExperimentPart
     properties(SetAccess=protected)
