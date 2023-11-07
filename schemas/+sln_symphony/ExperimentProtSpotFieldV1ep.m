1819410c785e24c5537d4b651247e1baf59a58ce
davidswygart <davidswygart2015@u.northwestern.edu>
2023-11-07 10:48:06 -0600

    Julia SpotField bp ep v1 new
    heheh

---
+++ schemas/+sln_symphony/ExperimentProtSpotFieldV1ep.m
@@ -0,0 +1,24 @@
+%{
+    #Epoch parameters for SpotField (1) 
+    -> sln_symphony.ExperimentEpoch
+    ---
+    cx : blob@raw
+    cy : blob@raw
+    
+%}
+
+classdef ExperimentProtSpotFieldV1ep < sln_symphony.ExperimentProtocol
+	properties
+
+		%attributes to be renamed
+		renamed_attributes = struct();
+
+		%attributes to be removed from the key
+		dropped_attributes = {'protocol_version'};
+	end
+	methods
+		function epoch_key = add_attributes(self, block_key, epoch_key) %#ok<INUSL,INUSD>
+		%add entities to the key based on others
+		end
+	end
+end
