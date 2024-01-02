0e006f5f5c05e01b5e12c20165451ba90f71d52e
Trung Tran-Le <38426554+trungtranle@users.noreply.github.com>
2024-01-02 13:38:42 -0600

    Update insert_experiment.m

--- schemas/+sln_symphony/insert_experiment.m
+++ schemas/+sln_symphony/insert_experiment.m
@@ -1,5 +1,14 @@
 function k = insert_experiment(exp_name)
 file_name = sprintf('%s.h5',exp_name);
 sln_animal.add_eyes_for_deceased_animals();
-k = insert(sln_symphony.Experiment,file_name);
+[k, success] = insert(sln_symphony.Experiment,file_name);
+if success
+    
+    disp('Inserted experiment successfully');
+    msgbox('Insert successful', 'Insert Done','help', 'modal');
+else
+    err_mess = sprintf('Insert experiment %s failed', file_name);
+    msgbox(err_mess, 'Insert Fail','error', 'modal');
+end
+
 %TODO: fix this so it returns the key...
