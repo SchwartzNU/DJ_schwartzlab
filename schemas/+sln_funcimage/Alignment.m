c5ea14fd8c74144c9f7bf947076ee6fde8dd5787
Greg Schwartz <greg.schwartz@northwestern.edu>
2024-03-07 16:23:50 -0600

    Update Alignment.m

--- schemas/+sln_funcimage/Alignment.m
+++ schemas/+sln_funcimage/Alignment.m
@@ -21,14 +21,21 @@ classdef Alignment < dj.Computed
             end
 
             epochs = sln_symphony.DatasetEpoch * sln_symphony.ExperimentEpoch & key;
-            
+            epoch_ids = fetchn(epochs,'epoch_id');
+            epoch_durations_ms = fetchn(epochs,'epoch_duration');
+            N_epochs = length(epoch_ids);
+
             V = zeros(image_props.height, image_props.n_frames);
+            func_volume = uint16(zeros(image_props.height, image_props.width, image_props.n_frames));
+            disp('Loading images');
             for i=1:image_props.n_frames
                 frame = imread([basedir image_props.alignment_fname],i);
                 V(:,i) = mean(frame,2);
+                func_volume(:,:,i) = imread([basedir image_props.image_fname]);
             end
             V_flat = reshape(V,[image_props.height*image_props.n_frames, 1]);
 
+            disp('Locating alignment pulses');
             alignment_pulse_trace = V_flat-min(V_flat);
             alignment_pulse_trace = alignment_pulse_trace./max(alignment_pulse_trace);
             pulses_up = getThresCross(alignment_pulse_trace, 0.5, 1);
@@ -37,8 +44,35 @@ classdef Alignment < dj.Computed
                 fprintf('Alignment error: number of alignment pulses (%d) did not match number of epochs (%d). \n', length(pulses_up), epochs.count);
                 return;
             end
-            keyboard;
+            decimal_frames = pulses_up / image_props.height;
+            start_frames = round(decimal_frames);
+            offsets = decimal_frames - start_frames;
+            offsets_ms = offsets / image_props.frame_rate * 1E3;            
+            dlmwrite([epoch_aligned_dir, filesep, 'ms_shifts.txt'], [epoch_ids, offsets_ms]);
+
+            info = imfinfo([basedir image_fname]);
             
+            disp('Writing aligned images');
+            for i=1:N_epochs
+                end_frame = start_frames(i) + ceil((epoch_durations_ms(i)/1E3)*image_props.frame_rate);
+
+                t = Tiff(sprintf('%s%sepoch_%d.tif', epoch_aligned_dir, filesep, epoch_ids(i)), 'w');
+
+                setTag(t,'Photometric',Tiff.Photometric.MinIsBlack);
+                setTag(t,'Compression',Tiff.Compression.None);
+                setTag(t,'BitsPerSample',info(1).BitDepth);
+                setTag(t,'SamplesPerPixel',end_frame(i) - start_frame(i) + 1);
+                setTag(t,'SampleFormat',Tiff.SampleFormat.UInt);
+                setTag(t,'ExtraSamples',Tiff.ExtraSamples.Unspecified);
+                setTag(t,'ImageLength',h);
+                setTag(t,'ImageWidth',w);
+                planarConfig = info(1).PlanarConfiguration;
+                setTag(t,'PlanarConfiguration',Tiff.PlanarConfiguration.(planarConfig));
+                write(t, func_volume(:,:,start_frames(i):end_frame));
+                close(t);
+            end   
+            self.insert(key)
+            disp('Insert successful.');
         end
     end
 
