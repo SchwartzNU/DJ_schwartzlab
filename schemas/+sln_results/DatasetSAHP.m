ef9d230c8fa0a941a2e818647c01883bb7ec5cd7
guardomayas <santigmaya@gmail.com>
2024-05-24 15:09:08 -0500

    Changing datatypes from result table

--- schemas/+sln_results/DatasetSAHP.m
+++ schemas/+sln_results/DatasetSAHP.m
@@ -9,12 +9,12 @@ ahp_amplitude=NULL : longblob # amplitude of after-hyperpolarization
 ahp_areas=NULL : longblob # vector with AHP areas for each epoch.
 auc_sp_ratios=NULL : longblob # vector with ahp_area over spike_count for each epoch
 entry_time = CURRENT_TIMESTAMP : timestamp # time the result was entered
-example_ahp=NULL : float # ahp area for the example traces
-example_diff=NULL : float # Vector with differences of AHP voltage and vrest.
+example_ahp=NULL : longblob # ahp area for the example traces
+example_diff=NULL : longblob # Vector with differences of AHP voltage and vrest.
 example_segment=NULL : float # Vector with the voltage of the post stimulation segment
 example_sp=NULL : longblob # Idk. Was drunk when I coded this
 example_spike_count=NULL : float # Vector of spike counts for the example traces
-example_traces=NULL : float # waveforms of example traces (mV). Usually the first epoch.
+example_traces=NULL : longblob # waveforms of example traces (mV). Usually the first epoch.
 git_tag : varchar(128) # git tag of current version of DJ_ROOT folder
 inj_current=NULL : longblob # vector of current injection amplitudes (pA)
 n_epochs_per_current=NULL : longblob # vector with how many trials for each injection size
