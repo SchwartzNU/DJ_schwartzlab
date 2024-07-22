45aabb62519745f92b3ae4488165870458005678
Greg Schwartz <greg.schwartz@northwestern.edu>
2024-07-22 16:52:45 -0500

    added try-catch for the functional imaging computed tables

--- schemas/+sln_funcimage/Alignment.m
+++ schemas/+sln_funcimage/Alignment.m
@@ -11,94 +11,97 @@ classdef Alignment < dj.Computed
 
     methods(Access=protected)
         function makeTuples(self, key)
-            pulse_on_stim = true;
-            
+            try
+                pulse_on_stim = true;
 
-            cellname = fetch1(sln_cell.CellName & key, 'cell_name');
-            basedir = [getenv('Func_imaging_folder') filesep 'SingleOrPairedCell' filesep cellname filesep];
-            image_props = fetch(sln_funcimage.ImagingRun & key, '*');
+                cellname = fetch1(sln_cell.CellName & key, 'cell_name');
+                basedir = [getenv('Func_imaging_folder') filesep 'SingleOrPairedCell' filesep cellname filesep];
+                image_props = fetch(sln_funcimage.ImagingRun & key, '*');
 
-            epoch_aligned_dir = [basedir extractBefore(image_props.image_fname, '.tif'), '_epochAligned'];
-            if ~isfolder(epoch_aligned_dir)
-                mkdir(epoch_aligned_dir);
-            end
-
-            epochs = sln_symphony.DatasetEpoch * sln_symphony.ExperimentEpoch & key;
-            epochs_struct = fetch(epochs);
-            if pulse_on_stim % need to add pre time
-                prot_name = fetch1(sln_symphony.ExperimentEpochBlock & epochs_struct(1),'protocol_name');
-                prot_name = sqlProtName2ProtName(prot_name);
-                example_epoch_params = fetch(aka.EpochParams(prot_name) * aka.BlockParams(prot_name) & epochs_struct(1), '*');
-                pre_time = example_epoch_params(1).pre_time;
-            end
-
-            epoch_ids = fetchn(epochs,'epoch_id');
-            epoch_durations_ms = fetchn(epochs,'epoch_duration');
-            N_epochs = length(epoch_ids);
-
-            V = zeros(image_props.height, image_props.n_frames);
-            %func_volume = uint16(zeros(image_props.height, image_props.width, image_props.n_frames));
-            disp('Loading images');
-            fprintf('%d frames to load.\n', image_props.n_frames);
-            for i=1:image_props.n_frames
-                if rem(i,100)==0
-                    fprintf('%d of %d frames loaded.\n', i, image_props.n_frames);
+                epoch_aligned_dir = [basedir extractBefore(image_props.image_fname, '.tif'), '_epochAligned'];
+                if ~isfolder(epoch_aligned_dir)
+                    mkdir(epoch_aligned_dir);
                 end
-                frame = imread([basedir image_props.alignment_fname],i);
-                V(:,i) = mean(frame,2);
-                func_volume(:,:,i) = imread([basedir image_props.image_fname],i);
+
+                epochs = sln_symphony.DatasetEpoch * sln_symphony.ExperimentEpoch & key;
+                epochs_struct = fetch(epochs);
+                if pulse_on_stim % need to add pre time
+                    prot_name = fetch1(sln_symphony.ExperimentEpochBlock & epochs_struct(1),'protocol_name');
+                    prot_name = sqlProtName2ProtName(prot_name);
+                    example_epoch_params = fetch(aka.EpochParams(prot_name) * aka.BlockParams(prot_name) & epochs_struct(1), '*');
+                    pre_time = example_epoch_params(1).pre_time;
+                end
+
+                epoch_ids = fetchn(epochs,'epoch_id');
+                epoch_durations_ms = fetchn(epochs,'epoch_duration');
+                N_epochs = length(epoch_ids);
+
+                V = zeros(image_props.height, image_props.n_frames);
+                %func_volume = uint16(zeros(image_props.height, image_props.width, image_props.n_frames));
+                disp('Loading images');
+                fprintf('%d frames to load.\n', image_props.n_frames);
+                for i=1:image_props.n_frames
+                    if rem(i,100)==0
+                        fprintf('%d of %d frames loaded.\n', i, image_props.n_frames);
+                    end
+                    frame = imread([basedir image_props.alignment_fname],i);
+                    V(:,i) = mean(frame,2);
+                    func_volume(:,:,i) = imread([basedir image_props.image_fname],i);
+                end
+                func_volume = func_volume - 2^15; %OFFSET when you save from FIJI for some reason
+                V_flat = reshape(V,[image_props.height*image_props.n_frames, 1]);
+
+                disp('Locating alignment pulses');
+                alignment_pulse_trace = V_flat-min(V_flat);
+                alignment_pulse_trace = alignment_pulse_trace./max(alignment_pulse_trace);
+                pulses_up = getThresCross(alignment_pulse_trace, 0.5, 1);
+
+                if epochs.count ~= length(pulses_up)
+                    fprintf('Alignment error: number of alignment pulses (%d) did not match number of epochs (%d). \n', length(pulses_up), epochs.count);
+                    return;
+                end
+                decimal_frames = pulses_up / image_props.height;
+                if pulse_on_stim
+                    decimal_frames = decimal_frames - (pre_time/1E3) * image_props.frame_rate;
+                end
+                start_frames = round(decimal_frames);
+                offsets = decimal_frames - start_frames;
+                offsets_ms = offsets / image_props.frame_rate * 1E3;
+                dlmwrite([epoch_aligned_dir, filesep, 'ms_shifts.txt'], [epoch_ids, offsets_ms]);
+
+                info = imfinfo([basedir image_props.image_fname]);
+
+                disp('Writing aligned images to database');
+
+                self.insert(key)
+                for i=1:N_epochs
+                    end_frame = start_frames(i) + ceil((epoch_durations_ms(i)/1E3)*image_props.frame_rate);
+
+                    % t = Tiff(sprintf('%s%sepoch_%d.tif', epoch_aligned_dir, filesep, epoch_ids(i)), 'w');
+                    % setTag(t,'Photometric',Tiff.Photometric.MinIsBlack);
+                    % setTag(t,'Compression',Tiff.Compression.None);
+                    % setTag(t,'BitsPerSample',info(1).BitDepth);
+                    % setTag(t,'SamplesPerPixel',end_frame - start_frames(i) + 1);
+                    % setTag(t,'SampleFormat',Tiff.SampleFormat.UInt);
+                    % setTag(t,'ExtraSamples',Tiff.ExtraSamples.Unspecified);
+                    % setTag(t,'ImageLength',image_props.height);
+                    % setTag(t,'ImageWidth',image_props.width);
+                    % planarConfig = info(1).PlanarConfiguration;
+                    % setTag(t,'PlanarConfiguration',Tiff.PlanarConfiguration.(planarConfig));
+                    % write(t, func_volume(:,:,start_frames(i):end_frame));
+                    % close(t);
+
+                    key_epoch_movie = key;
+                    key_epoch_movie = mergeStruct(key_epoch_movie, epochs_struct(i));
+                    key_epoch_movie.offset_ms = round(offsets_ms(i));
+                    key_epoch_movie.raw_movie = func_volume(:,:,start_frames(i):end_frame);
+                    insert(sln_funcimage.EpochMovie,key_epoch_movie);
+                end
+                disp('Insert successful.');
+            catch ME
+                disp('Alignment insert failed because of error:');
+                disp(ME.message);
             end
-            func_volume = func_volume - 2^15; %OFFSET when you save from FIJI for some reason
-            V_flat = reshape(V,[image_props.height*image_props.n_frames, 1]);
-
-            disp('Locating alignment pulses');
-            alignment_pulse_trace = V_flat-min(V_flat);
-            alignment_pulse_trace = alignment_pulse_trace./max(alignment_pulse_trace);
-            pulses_up = getThresCross(alignment_pulse_trace, 0.5, 1);
-            
-            if epochs.count ~= length(pulses_up)
-                fprintf('Alignment error: number of alignment pulses (%d) did not match number of epochs (%d). \n', length(pulses_up), epochs.count);
-                return;
-            end
-            decimal_frames = pulses_up / image_props.height;
-            if pulse_on_stim
-                decimal_frames = decimal_frames - (pre_time/1E3) * image_props.frame_rate;
-            end
-            start_frames = round(decimal_frames);
-            offsets = decimal_frames - start_frames;
-            offsets_ms = offsets / image_props.frame_rate * 1E3;            
-            dlmwrite([epoch_aligned_dir, filesep, 'ms_shifts.txt'], [epoch_ids, offsets_ms]);
-
-            info = imfinfo([basedir image_props.image_fname]);
-
-            disp('Writing aligned images to database');    
-            
-            self.insert(key)
-            for i=1:N_epochs
-                end_frame = start_frames(i) + ceil((epoch_durations_ms(i)/1E3)*image_props.frame_rate);
-
-                % t = Tiff(sprintf('%s%sepoch_%d.tif', epoch_aligned_dir, filesep, epoch_ids(i)), 'w');
-                % setTag(t,'Photometric',Tiff.Photometric.MinIsBlack);
-                % setTag(t,'Compression',Tiff.Compression.None);
-                % setTag(t,'BitsPerSample',info(1).BitDepth);
-                % setTag(t,'SamplesPerPixel',end_frame - start_frames(i) + 1);
-                % setTag(t,'SampleFormat',Tiff.SampleFormat.UInt);
-                % setTag(t,'ExtraSamples',Tiff.ExtraSamples.Unspecified);
-                % setTag(t,'ImageLength',image_props.height);
-                % setTag(t,'ImageWidth',image_props.width);
-                % planarConfig = info(1).PlanarConfiguration;
-                % setTag(t,'PlanarConfiguration',Tiff.PlanarConfiguration.(planarConfig));
-                % write(t, func_volume(:,:,start_frames(i):end_frame));
-                % close(t);
-
-                key_epoch_movie = key;
-                key_epoch_movie = mergeStruct(key_epoch_movie, epochs_struct(i));
-                key_epoch_movie.offset_ms = round(offsets_ms(i));
-                key_epoch_movie.raw_movie = func_volume(:,:,start_frames(i):end_frame);
-                insert(sln_funcimage.EpochMovie,key_epoch_movie);
-            end   
-            disp('Insert successful.');
-
         end
     end
 
--- schemas/+sln_funcimage/ImagingRun.m
+++ schemas/+sln_funcimage/ImagingRun.m
@@ -16,51 +16,56 @@ preprocessing_steps = null  : varchar(512) # comma separated list of (FIJI) prep
 classdef ImagingRun < dj.Manual
     methods
         function insert(self, key)
-            %key will be the primary key of the associated dataset
-            cellname = fetch1(sln_cell.CellName & key, 'cell_name');
-            basedir = [getenv('Func_imaging_folder') filesep 'SingleOrPairedCell' filesep cellname filesep];
-            disp('Select functional imaging data tif stack.');
-            image_fname = uigetfile('*.tif','Select functional imaging data tif stack.',basedir);
-            disp('Select associated alignment pulses tif stack.');
-            alignment_fname = uigetfile('*.tif','Select associated alignment pulses tif stack.',basedir);
-            disp('Select associated metadata .mat file.');
-            [meta_data_fname, meta_data_path] = uigetfile('*.mat','Select associated metadata .mat file.',basedir);
+            try
+                %key will be the primary key of the associated dataset
+                cellname = fetch1(sln_cell.CellName & key, 'cell_name');
+                basedir = [getenv('Func_imaging_folder') filesep 'SingleOrPairedCell' filesep cellname filesep];
+                disp('Select functional imaging data tif stack.');
+                image_fname = uigetfile('*.tif','Select functional imaging data tif stack.',basedir);
+                disp('Select associated alignment pulses tif stack.');
+                alignment_fname = uigetfile('*.tif','Select associated alignment pulses tif stack.',basedir);
+                disp('Select associated metadata .mat file.');
+                [meta_data_fname, meta_data_path] = uigetfile('*.mat','Select associated metadata .mat file.',basedir);
 
-            if all(meta_data_fname == false)
-                disp('Nothing inserted. Metadata required.');
-                return;
-            end
-            load([meta_data_path filesep meta_data_fname],'SI');
-            key.frame_rate = SI.hRoiManager.scanFrameRate;
+                if all(meta_data_fname == false)
+                    disp('Nothing inserted. Metadata required.');
+                    return;
+                end
+                load([meta_data_path filesep meta_data_fname],'SI');
+                key.frame_rate = SI.hRoiManager.scanFrameRate;
 
-            preprocessing_steps = inputdlg('List preprocessing steps (e.g. motion correction) (comma separated) or leave blank.',...
-                'Preprocessing steps',...
-                [1 60], ...
-                {''});
-            if ~isempty(preprocessing_steps)
-                preprocessing_steps = preprocessing_steps{1};
-            end
-            if all(image_fname == false)
-                disp('Nothing inserted. Image filename required.');
-                return;
-            end
+                preprocessing_steps = inputdlg('List preprocessing steps (e.g. motion correction) (comma separated) or leave blank.',...
+                    'Preprocessing steps',...
+                    [1 60], ...
+                    {''});
+                if ~isempty(preprocessing_steps)
+                    preprocessing_steps = preprocessing_steps{1};
+                end
+                if all(image_fname == false)
+                    disp('Nothing inserted. Image filename required.');
+                    return;
+                end
 
-            info = imfinfo([basedir image_fname]);
-            key.n_frames = length(info);
-            key.width = info(1).Width;
-            key.height = info(1).Height;
-            key.res_x = 1./info(1).XResolution;
-            key.res_y = 1./info(1).YResolution;
-            key.image_fname = image_fname;                
-            
-            if ~all(alignment_fname == false)
-                key.alignment_fname = alignment_fname;
+                info = imfinfo([basedir image_fname]);
+                key.n_frames = length(info);
+                key.width = info(1).Width;
+                key.height = info(1).Height;
+                key.res_x = 1./info(1).XResolution;
+                key.res_y = 1./info(1).YResolution;
+                key.image_fname = image_fname;
+
+                if ~all(alignment_fname == false)
+                    key.alignment_fname = alignment_fname;
+                end
+                if ~isempty(preprocessing_steps)
+                    key.preprocessing_steps = preprocessing_steps;
+                end
+                insert@dj.Manual(self, key);
+                disp('Insert successful.');
+            catch ME
+                disp('ImagingRun insert failed because of error:');
+                disp(ME.message);
             end
-            if ~isempty(preprocessing_steps)
-                key.preprocessing_steps = preprocessing_steps;
-            end            
-            insert@dj.Manual(self, key);
-            disp('Insert successful.');
         end
     end
 end
