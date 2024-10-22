f7269a0d17fc600a275c2f15120ab823fccb67f2
Le Trung <38426554+trungtranle@users.noreply.github.com>
2024-10-22 11:14:38 -0500

    add bp ep for whitenoisepulse

---
+++ schemas/+sln_symphony/ExperimentProtWhiteNoisePulseV1bp.m
@@ -0,0 +1,31 @@
+%{
+#b parameters for WhiteNoisePulse (1) 
+-> sln_symphony.ExperimentEpochBlock
+---
+output_amp_selection : smallint unsigned
+ramp_slope : float
+pre_time: float
+stim_time: float
+tail_time: float
+amplitude : float
+std : float
+frequency : float
+number_of_epochs : tinyint unsigned
+seed_start_value : smallint unsigned
+seed_change_mode : enum('repeat only', 'repeat & increment', 'increment only');
+%}
+classdef ExperimentProtWhiteNoisePulseV1bp < sln_symphony.ExperimentProtocol
+	properties
+
+		%attributes to be renamed
+		renamed_attributes = struct();
+
+		%attributes to be removed from the key
+		dropped_attributes = {};
+	end
+	methods
+		function block_key = add_attributes(self, block_key, epoch_key) %#ok<INUSL,INUSD>
+		%add entities to the key based on others
+		end
+	end
+end
