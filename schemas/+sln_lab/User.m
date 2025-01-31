878f5721504b6dc9bb4bc7b359ae4286a266e187
Greg Schwartz <greg.schwartz@northwestern.edu>
2025-01-31 11:25:27 -0600

    Update User.m

--- schemas/+sln_lab/User.m
+++ schemas/+sln_lab/User.m
@@ -3,6 +3,7 @@
 user_name : varchar(32)                  # Lab member name
 ---
 user_name_for_var : varchar(32)     # short version with no spaces or punctuation for a variable name
+net_id : varchar(16) #northwestern NETID
 %}
 classdef User < dj.Lookup
     
