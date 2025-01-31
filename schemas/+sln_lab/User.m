52e78c670a5d5d61d07786b095e4c2d35502cb62
Greg Schwartz <greg.schwartz@northwestern.edu>
2025-01-31 11:33:25 -0600

    Update User.m
    added netid to this table

--- schemas/+sln_lab/User.m
+++ schemas/+sln_lab/User.m
@@ -1,8 +1,9 @@
 %{
 # User: Lab member
-user_name : varchar(32)                  # Lab member name
+user_name                   : varchar(32)                   # Lab member name
 ---
-user_name_for_var : varchar(32)     # short version with no spaces or punctuation for a variable name
+user_name_for_var           : varchar(32)                   # short version with no spaces or punctuation for a variable name
+net_id=null                 : varchar(16)                   # northwestern netid
 %}
 classdef User < dj.Lookup
     
