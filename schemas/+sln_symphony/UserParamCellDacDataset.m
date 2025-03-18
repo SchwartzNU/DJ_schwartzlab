fe01f1db622b1aca63ccdf332871f01e635d2fb7
Greg Schwartz <greg.schwartz@northwestern.edu>
2025-03-18 12:32:54 -0500

    added a couple params

---
+++ schemas/+sln_symphony/UserParamCellDacDataset.m
@@ -0,0 +1,8 @@
+%{
+# UserParamCellDacDataset
+-> sln_symphony.ExperimentCell
+---
+dac_dataset = NULL : enum('T','F') # True if it is in the dAC dataset
+%}
+classdef UserParamCellDacDataset < dj.Manual
+end
---
+++ schemas/+sln_symphony/UserParamCellPatchSeq.m
@@ -0,0 +1,8 @@
+%{
+# UserParamCellPatchSeq
+-> sln_symphony.ExperimentCell
+---
+patch_seq = NULL : enum('T','F') # True if it has been sequenced
+%}
+classdef UserParamCellPatchSeq < dj.Manual
+end
