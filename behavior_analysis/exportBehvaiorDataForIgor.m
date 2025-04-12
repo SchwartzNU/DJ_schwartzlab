96b013e5738ca2c491e90f7a186a2ea009a8f7f7
Greg Schwartz <greg.schwartz@northwestern.edu>
2025-04-11 20:05:22 -0500

    Update exportBehvaiorDataForIgor.m

--- behavior_analysis/exportBehvaiorDataForIgor.m
+++ behavior_analysis/exportBehvaiorDataForIgor.m
@@ -8,6 +8,11 @@ S.sem_exp = std(S.exp_data) ./ sqrt(length(S.exp_data)-1);
 S.mean_control = mean(S.control_data);
 S.sem_control = std(S.control_data) ./ sqrt(length(S.control_data)-1);
 
-S.p = signrank(S.exp_data, S.control_data);
-
+if strcmp(paramName,'body_s')
+    test_vec = S.exp_data ./ S.control_data;
+    test_vec = test_vec - 1;
+    S.p = signtest(test_vec);
+else
+    S.p = signrank(S.exp_data, S.control_data);
+end
 exportStructToHDF5(S, [fname '.h5'], fname);
