165e0b76ad75fe1e99550a5aff41c06611332d37
Xin Zhang <xinzhang2020@u.northwestern.edu>
2025-04-29 10:42:57 -0500

    added an insert function for brainslicebatch

--- schemas/+sln_tissue/BrainSliceBatch.m
+++ schemas/+sln_tissue/BrainSliceBatch.m
@@ -9,7 +9,25 @@ thickness: smallint unsigned
 classdef BrainSliceBatch < dj.Manual
 methods
     function slicebatchid = addBrainSliceBatch(keys)
-        
+        checkanimal = "animal_id = " + keys.animal_id;
+        query = sln_tissue.BrainSliceBatch & checkanimal;
+        %check if the batch already exisits in datajoint
+        if exists(query)
+            fprintf('Brain slice batch already exits, function exiting...');
+            query
+            return;
+        end
+        %know try inserting
+        try
+            C = dj.conn;
+            C.startTransaction;
+            insert(sln_tissue.BrainSliceBatch, keys);
+            slicebatchid = fetch1(sln_tissue.BrainSliceBatch, 'tissue_id');
+            C.commitTransaction;
+        catch ME
+            C.cancelTransaction;
+            rethrow(ME)
+        end
     end
 end
 end
