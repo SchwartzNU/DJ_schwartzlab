177a5836e79e098a6209ed423fa23d513b428bb0
Trung <38426554+trungtranle@users.noreply.github.com>
2025-07-11 16:45:36 -0500

    Update extract_data_to_send_to_Jared_natural_motion_with_flash.m

--- Datajoint_io/extract_data_to_send_to_Jared_natural_motion_with_flash.m
+++ Datajoint_io/extract_data_to_send_to_Jared_natural_motion_with_flash.m
@@ -14,13 +14,13 @@ for c = cells'
         query = aka.EpochParams(prot) * aka.BlockParams(prot) * sln_symphony.ExperimentProjectorSettings *...
             sln_symphony.DatasetEpoch * sln_symphony.ExperimentChannel * sln_symphony.SpikeTrain * sln_cell.AssignType.current * sln_cell.Cell * ...
             (co & sprintf('source_id=%d', c))* proj(sln_symphony.ExperimentRetina, 'source_id->retina_id', 'side','orientation');
-            %* sln_symphony.Experiment * sln_symphony.Calibration; %for mpp
+        %* sln_symphony.Experiment * sln_symphony.Calibration; %for mpp
         query = proj(query,'IF(`orientation` like "ventral up",1,0) -> vu', 'IF(`side` like "left",1,0) -> le', '*');
         query = proj(query, 'vu xor le->flip', '*');
         query = proj(query, '(-2 * flip  + 1) * x -> NT', '*');
-        
+
         results(i).(prot) = fetch(query, '*');
-        
+
         if strcmp(prot,'NaturalMovingObjectAndFlash')
             %v2 -- inefficient since we're repeating seeds
             for j = 1: length(results(i).(prot))
@@ -28,33 +28,33 @@ for c = cells'
                 Nsamp = obj.pre_frames + obj.stim_frames + obj.tail_frames + 1;
                 total_time = Nsamp * 1/60;
                 t_hex = (obj.pre_frames + obj.stim_frames) * 1/60;
-    
-                if strcmp(obj.cur_motion_type, 'natural')
-                    [x,y] = sa_labs.util.BMARGSMv2(obj.tau, obj.sigma, obj.tauz,obj.sigmaz, 1/60, ...
-                        total_time,t_hex ,obj.mosaic_spacing/sqrt(3)+obj.leeway,obj.tburn,obj.motion_seed);
-                    
-                elseif strcmp(obj.cur_motion_type, 'control')
-                    [~,~,~,~,~,~,x,y,~,~] ...
-                            = sa_labs.util.BMARGSMv2(obj.tau, obj.sigma, obj.tauz,obj.sigmaz, 1/60, ...
-                        total_time,t_hex ,obj.mosaic_spacing/sqrt(3)+obj.leeway,obj.tburn,obj.motion_seed);
-                elseif strcmp(obj.cur_motion_type, 'flash')
-                    [~,~,~,~,~,~,~,~,x,y] = ...
-                            sa_labs.util.BMARGSMv2(obj.tau, obj.sigma, obj.tauz,obj.sigmaz, 1/60, ...
-                        total_time,t_hex ,obj.mosaic_spacing/sqrt(3)+obj.leeway,obj.tburn,obj.motion_seed);
-                end
-                
+
                 results(i).(prot)(j).trans_x = results(i).(prot)(j).cx;
                 results(i).(prot)(j).trans_y = results(i).(prot)(j).cy;
 
-                results(i).(prot)(j).cx = results(i).(prot)(j).cx + x(1:end-1);
-                results(i).(prot)(j).cy = results(i).(prot)(j).cy + y(1:end-1);
+                switch obj.cur_motion_type
+                    case 'flash'
+                        [x,y,~,~,~,~,~,~,~,~,x_hex,y_hex] = ...
+                            sa_labs.util.BMARGSMv2(obj.tau, obj.sigma, obj.tauz,obj.sigmaz, 1/60, ...
+                            total_time,t_hex ,obj.mosaic_spacing/sqrt(3)+obj.leeway,obj.tburn,obj.motion_seed);
+                        x(1:end) = x_hex;
+                        y(1:end) = y_hex;
+                    case 'natural'
+                        [x,y] = sa_labs.util.BMARGSMv2(obj.tau, obj.sigma, obj.tauz,obj.sigmaz, 1/60, ...
+                            total_time,t_hex ,obj.mosaic_spacing/sqrt(3)+obj.leeway,obj.tburn,obj.motion_seed);
+                    case 'control'
+                        [~,~,~,~,~,~,x,y,~,~] ...
+                            = sa_labs.util.BMARGSMv2(obj.tau, obj.sigma, obj.tauz,obj.sigmaz, 1/60, ...
+                            total_time,t_hex ,obj.mosaic_spacing/sqrt(3)+obj.leeway,obj.tburn,obj.motion_seed);
+                       
+                end
+                        results(i).(prot)(j).cx = results(i).(prot)(j).cx + x(1:end-1);
+                        results(i).(prot)(j).cy = results(i).(prot)(j).cy + y(1:end-1);
             end
-
-
         end
+        i = i+1;
     end
-    i = i+1;
 end
-%%
-save(out_file,'results', '-v7.3');
+    %%
+    save(out_file,'results', '-v7.3');
 
