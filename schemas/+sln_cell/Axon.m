423ff44946ab533e9d619d43a1e373ae1bb1b5e0
Xin Zhang <xinzhang2020@u.northwestern.edu>
2025-07-26 21:53:49 -0500

    trying to figure out computed table...

--- schemas/+sln_cell/Axon.m
+++ schemas/+sln_cell/Axon.m
@@ -9,14 +9,26 @@ side: enum('Ipsilateral', 'Contralateral', 'Unknown')
 %}
 classdef Axon < dj.Manual
     methods (Static)
-        function axon_id = add_axon(key)
+        function axon_id = add_axon(brain_region, side, cell_unid)
+            arguments
+                brain_region 
+                side 
+                cell_unid = NaN
+            end
             try
                 C = dj.conn;
                 C.startTransaction;
+                key.brain_region = brain_region;
+
+                key.side = side;
+
+                if (~isnan(cell_unid))
+                    key.cell_unid = cell_unid;
+                end
                 insert(sln_cell.Axon, key);
                 ids = fetch(sln_cell.Axon, 'axon_id');
                 axon_id = max([ids.axon_id]);
-                fpritnf('New axon added: %d\n', axon_id);
+                fprintf('New axon added: %d\n', axon_id);
             catch ME
                 rethrow (ME)
             end
--- schemas/+sln_cell/AxonCoordinate.m
+++ schemas/+sln_cell/AxonCoordinate.m
@@ -3,42 +3,32 @@
 -> sln_cell.Axon
 ---
 medial_lateral: double
-distance_from_1st_slice: double
+anterior_posterior: double
 %}
 
 classdef AxonCoordinate < dj.Computed
 properties (Constant)
-    keySource = sln_cell.Axon * sln_image.AxonImageAssociation * sln_image.AxonInBrain;
+    keySource = aggr( proj(sln_cell.Axon, 'axon_id') * sln_image.AxonImageAssociation * proj(sln_image.AxonInBrain, 'medial_lateral', 'distance_from_fist_slice'),...
+        proj(proj(sln_cell.Axon, 'axon_id') * sln_image.AxonImageAssociation * proj(sln_image.AxonInBrain, 'medial_lateral', 'distance_from_fist_slice'), 'axon_id'),...
+        'AVG(medial_lateral)->mean_ml', 'AVG(distance_from_fist_slice)->mean_ap ');
 end
 
 methods (Access=protected)
     function makeTuples(self, key)
+
+        data = fetch(self.keySource & key, '*');
+        % result  = key;
+        result.axon_id = key.axon_id;
+        result.medial_lateral = data.mean_ml;
+        result.anterior_posterior = data.mean_ap;
         
-        image_ids = fetch(sln_image.AxonImageAssociation & sprintf('axon_id = %d', key.axon_id), 'image_id');
-
-        all_ml = zeros([1, numel(image_ids)]);
-        all_ap = zeros([1, numel(image_ids)]);
-        for i = 1:numel(image_ids)
-            searchkey.image_id =  image_ids(i).image_id;
-            spinningdata = fetch(sln_image.AxonInBrain & searchkey, '*');
-            all_ml(i) = spinningdata.medial_lateral;
-            all_ap(i) = spinningdata.distance_from_fist_slice;
-        end
-        %the relative medial/lateral and anterior-posterior is the average of all the axinInBrain images that contains this axon 
-        key.medial_lateral = mean(all_ml);
-        key.distance_from_1st_slice = mean(all_ap);
-
-        inserting = {};
-        inserting.axon_id = key.axon_id;
-        inserting.medial_lateral = key.medial_lateral;
-        inserting.distance_from_1st_slice = key.distance_from_1st_slice;
+        %testing codes don't use
+        % key.medial_lateral = 0;
+        % key.anterior_posterior = 0;
 
         %try insert
-        try
-            self.insert(inserting);
-        catch ME
-            rethrow (ME);
-        end
+        %key = rmfield(key, 'image_id');
+        self.insert(result);
 
     end
 end
