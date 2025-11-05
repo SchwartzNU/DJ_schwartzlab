63dbbd8eee638f563727c50712a84c6dd0379f0f
Greg Schwartz <greg.schwartz@northwestern.edu>
2025-11-05 13:00:08 -0600

    Fixed a few bugs for Brain cells

--- schemas/+sln_cell/AssignBrainCellType.m
+++ schemas/+sln_cell/AssignBrainCellType.m
@@ -8,11 +8,11 @@
 classdef AssignBrainCellType < dj.Manual
     methods(Static)
         function type = current()
-            type = sln_cell.AssignBrainCellType * sln_cell.BrainCellEvent & 'LIMIT 1 PER cell_unid ORDER BY entry_time DESC';
+            type = sln_cell.AssignBrainCellType * sln_cell.CellEvent & 'LIMIT 1 PER cell_unid ORDER BY entry_time DESC';
         end
 
         function type = initial()
-            type = sln_cell.AssignBrainCellType * sln_cell.BrainCellEvent & 'LIMIT 1 PER cell_unid ORDER BY entry_time ASC';
+            type = sln_cell.AssignBrainCellType * sln_cell.CellEvent & 'LIMIT 1 PER cell_unid ORDER BY entry_time ASC';
         end
     end
 end
--- schemas/+sln_cell/add_unclassified_events.m
+++ schemas/+sln_cell/add_unclassified_events.m
@@ -11,15 +11,15 @@ if untyped_cells.exists
     end
 end
 
-untyped_brain_cells = sln_cell.BrainCell - (sln_cell.CellEvent * sln_cell.AssignType);
-if untyped_cells.exists
-    cell_unid = fetchn(untyped_cells,'cell_unid');
-    for i=1:untyped_cells.count
+untyped_brain_cells = sln_cell.BrainCell - (sln_cell.CellEvent * sln_cell.AssignBrainCellType);
+if untyped_brain_cells.exists
+    cell_unid = fetchn(untyped_brain_cells,'cell_unid');
+    for i=1:untyped_brain_cells.count
         key = struct; % just in case there is another name collision
         key.cell_class = 'other';
         key.cell_type = 'unclassified';
         key.cell_unid = cell_unid(i);
         key.user_name = 'Unknown';
-        sln_cell.add_event(key,'AssignType');
+        sln_cell.add_event(key,'AssignBrainCellType');
     end
 end
