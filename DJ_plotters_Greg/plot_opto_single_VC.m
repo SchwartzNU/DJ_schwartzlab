1cd8303f810f3f19d1a7e5c67e651e6b337c5064
Xin Zhang <xinzhang2020@u.northwestern.edu>
2025-11-18 11:07:20 -0600

    fixed plotter(?)

--- DJ_plotters_Greg/plot_opto_single_VC.m
+++ DJ_plotters_Greg/plot_opto_single_VC.m
@@ -1,4 +1,10 @@
-function tableData= plot_opto_single_VC(R,ax)
+function required_fields= plot_opto_single_VC(R,ax)
+
+if nargin < 1
+    required_fields = {'pre_time_ms', 'stim_time_ms', 'tail_time_ms', 'average_trace'};
+    return;
+end
+
 set(ax, 'XLim', [0, inf]);
 time_total = (R.pre_time_ms + R.stim_time_ms + R.tail_time_ms)/1E3;
 axis_x = linspace(0, time_total, time_total*R.sample_rate);
@@ -8,6 +14,6 @@ xline(R.pre_time_ms*R.sample_rate/1E3, '-', 'b');
 xline((R.pre_time_ms+R.stim_time_ms)*R.sample_rate/1E3, '--', 'b');
 ylabel(ax, 'Synpatic current (pA)');
 xbale(ax, 'Time(ms)');
-tableData.total_Epoch = R.epoch_total;
+
 
 end
