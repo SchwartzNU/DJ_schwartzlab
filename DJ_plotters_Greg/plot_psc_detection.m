5d67eb24e5679f8a2469711d6f82414c603b9d3e
Xin Zhang <xinzhang2020@u.northwestern.edu>
2025-11-20 11:29:58 -0600

    plotter for psc epoch detection
    create a simple plotter to check if psc detection is working

---
+++ DJ_plotters_Greg/plot_psc_detection.m
@@ -0,0 +1,27 @@
+function required_fields = plot_psc_detection(R, ax)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+if nargin < 1
+    required_fields = {'psc_start_ms', 'source_id', 'file_name', 'epoch_id', 'sample_rate'};
+    return;
+end
+
+%fetching raw trace from database
+q.file_name = R.file_name;
+q.source_id = R.source_id;
+q.epoch_id = R.epoch_id;
+%only plot Amp1 raw data, change if data is fron amp2
+q.channel_name = 'Amp1';
+data = fetch(sln_symphony.ExperimentElectrode&q, 'raw_data');
+
+%plotting the detection
+plot(ax, data.raw_data);
+hold(ax, 'on');
+[~, psc_n] = size(R.psc_start_ms);
+for i = 1:psc_n
+    timing = R.psc_start_ms/1000*R.sample_rate;
+    xline(timing, '-', 'Color', 'blue');
+end
+ylabel(ax, 'Synpatic current (pA)');
+hold(ax, 'off');
+end
