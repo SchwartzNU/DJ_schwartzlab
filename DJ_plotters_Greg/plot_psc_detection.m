4f75e63c382519e96d49557b367af34423ac2996
Xin Zhang <xinzhang2020@u.northwestern.edu>
2025-11-21 10:49:51 -0600

    mini fix for plotting psc detection
    also the time data in sln_results.EpochPostsynapticCurrent are seconds but name says miliseconds...

--- DJ_plotters_Greg/plot_psc_detection.m
+++ DJ_plotters_Greg/plot_psc_detection.m
@@ -7,7 +7,7 @@ if nargin < 1
 end
 
 %fetching raw trace from database
-q.file_name = R.file_name;
+q.file_name =char(R.file_name);
 q.source_id = R.source_id;
 q.epoch_id = R.epoch_id;
 %only plot Amp1 raw data, change if data is fron amp2
@@ -16,14 +16,21 @@ data = fetch(sln_symphony.ExperimentEpochChannel&q, 'raw_data');
 
 %plotting the detection
 
-x_axis = linspace(0, numel(data.raw_data)/sample_rate, numel(data.raw_data));
-plot(ax, x_axis, data.raw_data);
+x_axis = linspace(0, numel(data.raw_data)/R.sample_rate, numel(data.raw_data));
+%plot(x_axis, data.raw_data, 'Color', 'black');
+plot(ax, x_axis, data.raw_data, 'Color', 'black');
+%hold on;
 hold(ax, 'on');
 [~, psc_n] = size(R.psc_start_ms);
 for i = 1:psc_n
-    timing = R.psc_start_ms/1000*R.sample_rate;
+    timing = R.psc_start_ms(i);
+    %xline(timing, '-', 'Color', 'blue');
     xline(ax, timing, '-', 'Color', 'blue');
 end
 ylabel(ax, 'Synpatic current (pA)');
+xlabel(ax, 'Time (s)');
+set(ax,'XtickMode','auto');
+set(ax,'YtickMode','auto');
 hold(ax, 'off');
+%hold off;
 end
