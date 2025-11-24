14862428263b67f853e777235b33d62daee91d46
Xin Zhang <xinzhang2020@u.northwestern.edu>
2025-11-24 13:04:35 -0600

    fixed a typo in table DatasetPscOptoStats

--- schemas/+sln_results/DatasetPscOptoStats.m
+++ schemas/+sln_results/DatasetPscOptoStats.m
@@ -10,7 +10,7 @@ git_tag : varchar(128) # git tag of current version of DJ_ROOT folder
 if_multi_pulse : int unsigned # 1 if train, 0 if not
 latency_opto_ms=NULL : longblob # the timing diff between psc and previous opto stim onset. If psc occurs before any opto onset, it will be negative
 opto_duration_ms : int unsigned # the duration of the said opt
-pcs_frequency=NULL : float # number of psc/total time elapsed
+psc_frequency=NULL : float # number of psc/total time elapsed
 psc_amp_mean=NULL : float # the average peak value of psc
 psc_risetime_mean_s=NULL : float # average rise time
 psc_total_dataset : int unsigned # total number of detected psc
