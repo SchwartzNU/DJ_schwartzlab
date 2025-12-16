76da3edd9c5b91674f1b1b2d982104bb29023c86
Xin Zhang <xinzhang2020@u.northwestern.edu>
2025-12-16 14:46:11 -0600

    added a small function to insert a cell and return the cell_unid

--- schemas/+sln_cell/Cell.m
+++ schemas/+sln_cell/Cell.m
@@ -6,4 +6,18 @@ cell_unid                   : int unsigned AUTO_INCREMENT   #
 -> [nullable, unique] sln_symphony.ExperimentCell
 %}
 classdef Cell < dj.Manual
+    methods(Static)
+        %this function currently works for cells NOT related to electrophsiology experiment
+        function cell_id = insert_get_id(animal)
+            cell.animal_id = animal;
+            insert(cell);
+
+            %now we have to query the cell out....
+            cells = fetch(sln_cell.Cell & cell);
+            %usually the newest one
+            sorted_ids = sort([cell.cell_unid], 'descend');
+            cell_id = sorted_ids(1);
+            fprintf('Added new cell: %d\n', cell_id);
+        end
+    end
 end
