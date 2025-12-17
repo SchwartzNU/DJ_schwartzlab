3c85d8a2ef81e6b432a43caf331b592fe12470c5
Xin Zhang <xinzhang2020@u.northwestern.edu>
2025-12-17 10:49:32 -0600

    bugs

--- schemas/+sln_cell/Cell.m
+++ schemas/+sln_cell/Cell.m
@@ -10,12 +10,12 @@ classdef Cell < dj.Manual
         %this function currently works for cells NOT related to electrophsiology experiment
         function cell_id = insert_get_id(animal)
             cell.animal_id = animal;
-            insert(cell);
+            insert(sln_cell.Cell, cell);
 
             %now we have to query the cell out....
             cells = fetch(sln_cell.Cell & cell);
             %usually the newest one
-            sorted_ids = sort([cell.cell_unid], 'descend');
+            sorted_ids = sort([cells.cell_unid], 'descend');
             cell_id = sorted_ids(1);
             fprintf('Added new cell: %d\n', cell_id);
         end
