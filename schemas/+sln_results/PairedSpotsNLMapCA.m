0af1a2bedc58952de246c0ebad948484c1daa408
Greg Schwartz <greg.schwartz@northwestern.edu>
2025-12-21 12:04:12 -0600

    fixed class name

--- schemas/+sln_results/PairedSpotsNLMapCA.m
+++ schemas/+sln_results/PairedSpotsNLMapCA.m
@@ -1,14 +1,14 @@
 %{
-# PairedSpotsNLMap_CA
+# PairedSpotsNLMapCA
 -> sln_symphony.Dataset
 ---
 analysis_name                   : varchar(128) # name of analysis
 analysis_entry_time = CURRENT_TIMESTAMP : timestamp # when this was entered into db
 git_tag : varchar(128) # git tag of current version of DJ_ROOT folder
 %}
-classdef PairedSpotsNLMap_CA < dj.Computed
+classdef PairedSpotsNLMapCA < dj.Computed
     properties
-        keySource = sln_reuslts.DatasetPairedSpotsCA;
+        keySource = sln_results.DatasetPairedSpotsCA;
     end
      methods(Access=protected)
         function makeTuples(self, key)
