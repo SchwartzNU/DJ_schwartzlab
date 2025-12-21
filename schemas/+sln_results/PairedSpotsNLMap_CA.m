23d0786865926c2a82e524ac0663136ae5e1f0ac
Greg Schwartz <greg.schwartz@northwestern.edu>
2025-12-21 12:01:21 -0600

    Create PairedSpotsNLMap_CA.m

---
+++ schemas/+sln_results/PairedSpotsNLMap_CA.m
@@ -0,0 +1,21 @@
+%{
+# PairedSpotsNLMap_CA
+-> sln_symphony.Dataset
+---
+analysis_name                   : varchar(128) # name of analysis
+analysis_entry_time = CURRENT_TIMESTAMP : timestamp # when this was entered into db
+git_tag : varchar(128) # git tag of current version of DJ_ROOT folder
+%}
+classdef PairedSpotsNLMap_CA < dj.Computed
+    properties
+        keySource = sln_reuslts.DatasetPairedSpotsCA;
+    end
+     methods(Access=protected)
+        function makeTuples(self, key)
+            disp('populing NL map')
+            key
+
+        end
+     end
+
+end
