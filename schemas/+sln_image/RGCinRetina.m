08594aac6c3ad48b306e599dc5245414698501d1
Xin Zhang <xinzhang2020@u.northwestern.edu>
2026-01-16 14:41:50 -0600

    reworked table for spinning disk rgc image
    - removed the coordinates information ... currently saved in sln_tissue.RetinRecon
    - simplified input function
    - mask.tif now are stored as raw data

--- schemas/+sln_image/RGCinRetina.m
+++ schemas/+sln_image/RGCinRetina.m
@@ -2,99 +2,94 @@
 #Annotate the high-resolution spinning disk image of a RGC 
 ->sln_image.Image
 -----
-(whole_retina)->sln_image.Image #the image id of the whole retina image that this high-resolution image is associated with
 background_roi: blob@raw #the 4 lines that defines the background
-centroid_x: double
-centroid_y: double
-radius: double
 color_pixel: blob@raw
-maskpath: varchar(512)
-->sln_cell.Cell #link to the cell, which should be created when uploading 
+mask_rgc:blob@raw
 %}
 
 classdef RGCinRetina < dj.Manual
 methods (Static)
     %centroid needs to be a structure with field: x, y, r. 
-    function image_id = upload_rgc_in_retina(filename, user, scope, channel, z_scale, wholeretina_imid, centroid, cellid,interleave, imageid)
-        arguments
-            filename 
-            user 
-            scope 
-            channel 
-            z_scale 
-            wholeretina_imid 
-            centroid 
-            cellid = NaN;
-            
-            interleave = false;
-            imageid = NaN;
-        end
-        try
-            if (isnan(imageid))
-                sln_image.Image.LoadFromFilewithStructuralInput(filename, user, scope, channel, z_scale, interleave);
-
-                [folderPath, filepre, fileext] = fileparts(filename);
-                query = {};
-                query.image_filename = append(filepre, fileext);
-                query.folder = folderPath;
-                query.image_filename = convertStringsToChars(query.image_filename);
-                query.folder = convertStringsToChars(query.folder);
-                result = fetch(sln_image.Image & query, 'image_id');
-                fprintf('RGC image uploaded, image id: %d', result.image_id);
-                image_id = result.image_id;
-            else
-                fprintf('Image already uploaded %d, creating RGC annotation now...\n', imageid);
-                result.image_id = imageid;
-            end
-
-
-            mask = fullfile(folderPath, 'mask.tif');
-            if (~isfile(mask))
-                fprintf('Upload failed, see error message');
-                error('Cannot find mask file in the folder');
-            end
-            %back ground could be either .roi or .mat
-            background = fullfile(folderPath, 'background.roi');
-            if (~isfile(background))
-                background = fullfile(folderPath, 'background.mat');
-                if (~isfile(background))
-                    error('Cannot find background file in the same folder as image!');
-                end
-            end
-
-            %start uploading sln_image.WholeRetinaImage
-            if (~isnan(cellid))
-                fprintf('Inserting spinning disk images for cell: %d', cellid);
-                sln_image.RGCinRetina.assign_rgc_in_retina(result.image_id, wholeretina_imid, centroid, mask, cellid, background);
-                fprintf('Sucess!');
-            else
-                %creating sln_cell.Cell entries, may cause trouble down the line
-                query = {};
-                query.image_id = wholeretina_imid;
-                mouse = fetch(sln_image.WholeRetinaImage & query, 'animal_id');
-                %creating cells...
-                newcell.animal_id = mouse.animal_id;
-                C = dj.conn;
-                C.startTransaction;
-                insert(sln_cell.Cell, newcell);
-                C.commitTransaction;
-
-                %query the newly inserted cells
-                cells = fetch(sln_cell.Cell & newcell, 'cell_unid');
-                [~, idx] = sort([cells.cell_unid], 'descend');
-                newid = cells(idx(1)).cell_unid;
-                fprintf('New cell %d created for image %s', newid, filename);
-                sln_image.RGCinRetina.assign_rgc_in_retina(result.image_id, wholeretina_imid, centroid, mask,newid, background);
-                fprintf('Image inserted!');
-            end
-           
- 
-        catch ME
-            fprintf('uploading rgc image failed!')
-            rethrow(ME);
-        end
-
-    end
+    % function image_id = upload_rgc_in_retina(filename, user, scope, channel, z_scale, wholeretina_imid, centroid, cellid,interleave, imageid)
+    %     arguments
+    %         filename 
+    %         user 
+    %         scope 
+    %         channel 
+    %         z_scale 
+    %         wholeretina_imid 
+    %         centroid 
+    %         cellid = NaN;
+    % 
+    %         interleave = false;
+    %         imageid = NaN;
+    %     end
+    %     try
+    %         if (isnan(imageid))
+    %             sln_image.Image.LoadFromFilewithStructuralInput(filename, user, scope, channel, z_scale, interleave);
+    % 
+    %             [folderPath, filepre, fileext] = fileparts(filename);
+    %             query = {};
+    %             query.image_filename = append(filepre, fileext);
+    %             query.folder = folderPath;
+    %             query.image_filename = convertStringsToChars(query.image_filename);
+    %             query.folder = convertStringsToChars(query.folder);
+    %             result = fetch(sln_image.Image & query, 'image_id');
+    %             fprintf('RGC image uploaded, image id: %d', result.image_id);
+    %             image_id = result.image_id;
+    %         else
+    %             fprintf('Image already uploaded %d, creating RGC annotation now...\n', imageid);
+    %             result.image_id = imageid;
+    %         end
+    % 
+    % 
+    %         mask = fullfile(folderPath, 'mask.tif');
+    %         if (~isfile(mask))
+    %             fprintf('Upload failed, see error message');
+    %             error('Cannot find mask file in the folder');
+    %         end
+    %         %back ground could be either .roi or .mat
+    %         background = fullfile(folderPath, 'background.roi');
+    %         if (~isfile(background))
+    %             background = fullfile(folderPath, 'background.mat');
+    %             if (~isfile(background))
+    %                 error('Cannot find background file in the same folder as image!');
+    %             end
+    %         end
+    % 
+    %         %start uploading sln_image.WholeRetinaImage
+    %         if (~isnan(cellid))
+    %             fprintf('Inserting spinning disk images for cell: %d', cellid);
+    %             sln_image.RGCinRetina.assign_rgc_in_retina(result.image_id, wholeretina_imid, centroid, mask, cellid, background);
+    %             fprintf('Sucess!');
+    %         else
+    %             %creating sln_cell.Cell entries, may cause trouble down the line
+    %             query = {};
+    %             query.image_id = wholeretina_imid;
+    %             mouse = fetch(sln_image.WholeRetinaImage & query, 'animal_id');
+    %             %creating cells...
+    %             newcell.animal_id = mouse.animal_id;
+    %             C = dj.conn;
+    %             C.startTransaction;
+    %             insert(sln_cell.Cell, newcell);
+    %             C.commitTransaction;
+    % 
+    %             %query the newly inserted cells
+    %             cells = fetch(sln_cell.Cell & newcell, 'cell_unid');
+    %             [~, idx] = sort([cells.cell_unid], 'descend');
+    %             newid = cells(idx(1)).cell_unid;
+    %             fprintf('New cell %d created for image %s', newid, filename);
+    %             sln_image.RGCinRetina.assign_rgc_in_retina(result.image_id, wholeretina_imid, centroid, mask,newid, background);
+    %             fprintf('Image inserted!');
+    %         end
+    % 
+    % 
+    %     catch ME
+    %         fprintf('uploading rgc image failed!')
+    %         rethrow(ME);
+    %     end
+    % 
+    % end
     function pixel_color = extract_single_frame(image_frame, bg_line, mask_frame)
         [row, col] = find(mask_frame~=0);
         indx = sub2ind(size(image_frame), row, col);
@@ -113,14 +108,12 @@ methods (Static)
             pixel_color(:, c) = filteredframe-background;
         end
     end
-    function assign_rgc_in_retina(imageid, wholeRetina, centroid,  maskpath, cellid, background, color)
+    function assign_rgc_in_retina(imageid, cellid, background,  maskpath, color)
         arguments
-            imageid 
-            wholeRetina 
-            centroid 
-            maskpath 
+            imageid               
             cellid 
-            background
+            background = NaN
+            maskpath = NaN
             color = NaN;
         end
         try
@@ -131,19 +124,41 @@ methods (Static)
 
             if (~exists(query1))
                 error('Image not found in the sln_image.Image, upload first.');
+                
+            end
+            %get background from folder in sln_image.Image if left empty
+            imdata = fetch(query1, 'folder');
+            if (isnan(background))
+                background = fullfile(imdata.folder, 'background.roi');
+                if (~isfile(background))
+                    error('Cannot find background roi in the folder by table sln_image: %s\n', background);
+                end
+            end 
+
+            if (isnan(maskpath))
+                maskpath = fullfile(imdata.folder, 'mask.tif');
+                if (~isfile(maskpath))
+                    error('Cannot find mask.tif in folder by sln_image.Image: %s\n', imdata.folder);
+                end
+            end
+           
+            %linking image with cell
+            assokey.cell_unid = cellid;
+            assokey.image_id = imageid;
+            asso = fetch(sln_image.RetinalCellImage & assokey);
+            C = dj.conn;
+            C.startTransaction;
+
+            if (isempty(asso))
+                fprintf('Adding new aasociation between cell %d and image %d\n', cellid, imageid);
+                insert(sln_image.RetinalCellImage, assokey);
+            else
+                fprintf('Image and rgc association already exists!\n');
+                disk(assokey);
             end
 
-            query2 = sln_image.RGCinRetina & key;
-            key.whole_retina = wholeRetina;
-            if (exists(query2))
-                fprintf('Image already annotated!');
-                return
-            end
+            
 
-            %build other parts of the key
-            key.centroid_x = centroid.x;
-            key.centroid_y = centroid.y;
-            key.radius = centroid.r;
            
             if (endsWith(background, '.roi'))
                 roi = ReadImageJROI(background);
@@ -168,8 +183,9 @@ methods (Static)
             infopack = imfinfo(maskpath);
             slice_total = numel(infopack);
 
-            %im_h = infopack(1).Height;
-           % im_w = infopack(1).Width;
+            im_h = infopack(1).Height;
+           im_w = infopack(1).Width;
+           mask_data =  uint8(zeros(im_h, im_w, slice_total));
 
             %reduce the value of true pixel in mask to 1
             %mask_ar = mask_ar/max(mask_ar, [], 'all');
@@ -185,7 +201,8 @@ methods (Static)
                     %loop through all the slices of the z stack image, not
                     %idea but only once
                     %get mask into numeric array and pixel colors
-                    maskframe = imread(maskpath, s);                                     
+                    maskframe = imread(maskpath, s);
+                    mask_data(:, :, s) = imread(maskpath, index = s);
                     
                     color{end+1} = sln_image.RGCinRetina.extract_single_frame(data.raw_image(:, :, s, :), key.background_roi, maskframe);
                     %pixel_color = extract_single_frame(image_frame, bg_line, mask_frame)
@@ -194,10 +211,10 @@ methods (Static)
             end
 
             key.color_pixel = color;
+            key.mask_rgc = mask_data;
             
             key.cell_unid = cellid;
-            C = dj.conn;
-            C.startTransaction;
+
             insert(sln_image.RGCinRetina, key);
             C.commitTransaction;
             fprintf('Insert successful:')
