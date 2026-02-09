69ded30d6884bc41f9388d1b51fad1ad7c442b4b
Xin Zhang <xinzhang2020@u.northwestern.edu>
2026-02-09 17:48:12 -0600

    Fixed cell number limit in morphology upload
    previously sln_image.LoadMorphologyFromFolder only works for 1-1 cell-image pair. Now it will work for 1 image-multi cell data. Need keyboard input to chose which cell this morphology data file is for.

--- schemas/+sln_image/loadMorphologyFromFolder.m
+++ schemas/+sln_image/loadMorphologyFromFolder.m
@@ -90,7 +90,78 @@ for i=1:length(D)
                 disp('Done');
             end
         else %multiple matches found, error
-            fprintf('Error arborData in folder %s matched to more than one cell in database\n', cur_folder);
+            fprintf('Warning: arborData in folder %s matched to more than one cell in database\n', cur_folder);
+            cell_ids = fetch(matched_cell, 'cell_unid');
+            fprintf('Those cells are both/all linked to the same image, please chose one after the promot appears.\n');
+            for k = 1:numel(cell_ids)
+                fprintf('%d\n', cell_ids(k).cell_unid);
+            end
+            fprintf('Hint: to terminate this function press ctrl+C\n');
+            inputconfirmed = false;
+            while ~inputconfirmed
+                input1 = input('Type the cell id: ');
+                
+                if (isnumeric(input1))
+                    if(ismember(input1, [cell_ids.cell_unid]))
+                        input2 = input('Confirm? press Y. If not, press any other key\n', "s");
+                        if (strcmpi(input2, 'y'))
+                            inputconfirmed = true;
+                            break;
+                        else
+                            frpintf('Cell id not confirmed... redo\n');
+                        end
+                    end
+                    fprintf('Please type the one of the cell id\n');
+                end
+                fprintf('Input is not a number\n');
+            end
+            
+            %upload processs... 
+            fprintf('Matched arborData for image %s to cell %d\n', match_name, input1);
+            newq = sprintf('cell_unid = %d', input1);
+            %sanity check: has the morphology been uploaded before?
+            morph = fetch(sln_image.RetinalCellMorphology & newq);
+            if (numel(morph)~=0)
+                fprintf('Data already exist, existing the function now\n');
+                return
+            end
+
+            matched_cell = sln_cell.RetinalCell * sln_image.RetinalCellImage & newq;
+            load([cur_folder filesep 'arborData.mat'],'appdata');
+            key = struct;
+            key.cell_unid = fetch1(matched_cell,'cell_unid');
+            key.nodes_flattened = appdata.nodes_flattened;
+            key.edges_flattened = appdata.edges_flattened;
+            key.radii_flattened = appdata.radii_flattened;
+            key.lower_surface_z = appdata.lower_surface_z;
+            key.upper_surface_z = appdata.upper_surface_z;
+            key.strat_x = appdata.strat_x;
+            key.strat_density = appdata.strat_density;
+            key.strat_y_norm = appdata.strat_y_norm;
+            key.branch_lengths = appdata.arborStats.branchLen;
+            key.branch_angles = appdata.arborStats.branchAngle;
+            key.branch_z_range = appdata.arborStats.branchRangeZ;
+            key.branch_tortuosity = appdata.arborStats.branchTortuosity;
+            key.n_branches = appdata.arborStats.Nbranches;
+            key.arbor_length = appdata.arborStats.totalLen;
+            key.arbor_complexity = appdata.arborStats.arborComplexity;
+            key.bistratified = appdata.arborStats.bistratified;
+            if key.bistratified
+                key.polygon_area_lower = appdata.arborStats.polygonArea_lower;
+                key.convexity_index_lower = appdata.arborStats.convexityIndex_lower;
+                key.polygon_area_upper = appdata.arborStats.polygonArea_upper;
+                key.convexity_index_upper = appdata.arborStats.convexityIndex_upper;
+                key.arbor_density_upper = appdata.arborStats.arborDensity_upper;
+                key.arbor_density_lower = appdata.arborStats.arborDensity_lower;
+            else
+                key.polygon_area = appdata.arborStats.polygonArea;
+                key.convexity_index = appdata.arborStats.convexityIndex;
+                key.arbor_density = appdata.arborStats.arborDensity;
+            end
+            disp('Inserting sln_image.RetinalCellMorphology');
+            insert(sln_image.RetinalCellMorphology,key);
+            disp('Done');
+
         end
     end
 end
