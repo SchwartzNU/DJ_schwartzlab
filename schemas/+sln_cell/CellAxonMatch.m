442f7b8a0092696cca0ac34d694d0d2084db141c
Xin Zhang <xinzhang2020@u.northwestern.edu>
2026-02-24 16:30:56 -0600

    simple function for updating table sln_cell.CellAxonMatch

--- schemas/+sln_cell/CellAxonMatch.m
+++ schemas/+sln_cell/CellAxonMatch.m
@@ -9,4 +9,39 @@ certainty: enum('Certain', 'Uncertain')
 %}
 
 classdef CellAxonMatch <dj.Manual
+    methods (Static)
+        function insertMatch(axonId, cellId, userName, certainty)
+            key.axon_id = axonId;
+            key.cell_unid = cellId;
+            key.user_name = userName;
+            key.certainty = certainty;
+
+            %check if there is already one
+            result = fetch(sln_cell.CellAxonMatch & key);
+            if (~isempty(result))
+                warning('This axon - cell pair already exists!\n');
+                return
+            end
+
+            try
+                C = dj.conn;
+                C.startTransaction;
+                insert(sln_cell.CellAxonMatch, key);
+                C.commitTransaction;
+                fprintf('Insert successful! Cell %d is associated with axon(s) below:\n', cellId);
+
+                qs = sprintf('cell_unid = %d', cellId);
+                allaxons = fetch(sln_cell.CellAxonMatch & qs);
+                for i = 1:length(allaxons)
+                    fprintf('Axon ID: %d\n', allaxons(i).axon_id);
+                end
+
+            catch ME
+                throw(ME);
+            end
+
+            % Insert a new match entry into the database
+
+        end
+    end
 end
