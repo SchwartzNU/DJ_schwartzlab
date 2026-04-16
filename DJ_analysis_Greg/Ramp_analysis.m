340739e13acd8b2bbb083b1e39e3276b0fb70cd0
Le Trung Tran <letrungtran@trungs-mbp.fsm.northwestern.edu>
2026-04-16 09:51:36 -0500

    updates Ramp

--- DJ_analysis_Greg/Ramp_analysis.m
+++ DJ_analysis_Greg/Ramp_analysis.m
@@ -4,9 +4,7 @@ datasets = aka.Dataset & data_group;
 datasets_struct = fetch(datasets);
 N_datasets = datasets.count;
 
-%need to add the table definition from excel file
-%here
-
+R = sln_results.table_definition_from_template('Ramp_analysis',N_datasets);
 
 for d=1:N_datasets
     tic;
@@ -40,22 +38,26 @@ for d=1:N_datasets
     % plot(raw_data');
     % subplot(2,1,2);
     % plot(stimulus);
-    
+
     %% Tail time analysis
     tau_1 = nan(size(raw_data,1), 1);
     tau_2 = nan(size(raw_data,1), 1);
+    max_ahp = nan(size(raw_data, 1), 1);
+    rheobase = nan(size(raw_data, 1), 1);
+    max_spike_freq = nan(size(raw_data, 1), 1);
+    block_current = nan(size(raw_data, 1), 1);
 
-    if tail_time > 2
-        fit_time = 2;
+
+    if tail_time > 1
+        fit_time = 1;
     else
         fit_time = tail_time-0.5;
+
     end
-
     for i = 1 : size(raw_data,1)
-
+        %post_stim_AHP
         post_stim_data = raw_data(i, ((pre_time + stim_time) * sample_rate) : ((pre_time + stim_time + 0.2) * sample_rate));
-
-        [~, idx] = min(post_stim_data);
+        [max_ahp(i), idx] = min(post_stim_data);
         idx = idx + (pre_time + stim_time) * sample_rate;
         y = raw_data(i, idx : (idx + (fit_time * sample_rate)));
         t = [0:length(y)-1] / sample_rate;
@@ -71,14 +73,53 @@ for d=1:N_datasets
         [fitresult, gof] = fit( xData, yData, ft, opts );
         tau_1(i) = -1/fitresult.b;
         tau_2(i) = -1/fitresult.d;
+
+        %find current for first peak
+        pre_time_point = pre_time * sample_rate;
+        stim_end_point = (pre_time + stim_time) * sample_rate;
+
+        spike_idx = [epochs_in_dataset(i).spike_indices];
+        if isempty(spike_idx)
+            rheobase(i) = 9999;
+            block_current(i) =  9999;
+        elseif spike_idx(1) < pre_time_point
+            rheobase(i) = 0;
+        else
+            first_spike = spike_idx(1);
+            rheobase(i) = stimulus(first_spike);
+        end
+
+        if isempty(spike_idx)
+            max_spike_freq(i) = 0;
+        else
+            spikes_idx_stim = find((spike_idx > pre_time_point) & (spike_idx < stim_end_point));
+            spikes_stim = spike_idx(spikes_idx_stim);
+            spike_instantaneous_freq = 1 ./ (double((spikes_stim(2:end) - spikes_stim(1:end-1))) ./ sample_rate);
+            max_spike_freq(i) = max(spike_instantaneous_freq);
+            block_current(i) = stimulus(spikes_stim(end));
+        end
+
+
+
+
     end
-    % fitresult.a
-    % mean(tau_1) 
-    % std(tau_1) 
-    % fitresult.c
-    % mean(tau_2) 
-    % std(tau_2)
-    
-    min(mean(tau_1), mean(tau_2))
+    R.file_name{d} = datasets_struct(d).file_name;
+    R.dataset_name{d} = datasets_struct(d).dataset_name;
+    R.source_id(d) = datasets_struct(d).source_id;
+    R.mean_trace{d} = mean_trace;
+    R.stimulus{d} = stimulus;
+    R.tau_1_mean(d) = mean(tau_1);
+    R.tau_2_mean(d) = mean(tau_2);
+    R.tau_1_sd(d) = std(tau_1);
+    R.tau_2_sd(d) = std(tau_2);
+    R.ahp_max_mean(d) = mean(max_ahp);
+    R.ahp_max_std(d) = std(max_ahp);
+    R.n_trial(d) = size(raw_data,1);
+    R.resting_vm(d) = mean(resting_vm, 'omitmissing');
+    R.rheobase{d} = [rheobase];
+    R.rheobase_mean(d) = mean(rheobase, "omitmissing");
+    R.rheobase_std(d) = std(rheobase, 'omitmissing');
+    R.max_spike_frequency_mean(d) = mean(max_spike_freq);
+    R.block_current(d) = mean(block_current);
 end
 end
