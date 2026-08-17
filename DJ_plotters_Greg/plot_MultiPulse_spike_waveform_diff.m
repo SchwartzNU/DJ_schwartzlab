1540695aa150b7efb5cc7c3dbffe3f14b2b74a30
Le Trung <38426554+trungtranle@users.noreply.github.com>
2026-08-17 14:57:58 -0500

    Add plots

--- DJ_plotters_Greg/plot_MultiPulse_spike_waveform_diff.m
+++ DJ_plotters_Greg/plot_MultiPulse_spike_waveform_diff.m
@@ -1,21 +1,21 @@
-function required_fields = plot_MultiPulse_spike_waveform(R,ax)
+function required_fields = plot_MultiPulse_spike_waveform_diff(R,ax)
 if nargin < 1
     required_fields = {'average_spike', ...
-     'time_vector', ...
      'spike_diff'};
     return;
 end
 
-time_vector = R.time_vector;
+
 average_spike = R.average_spike;
+spike_diff = R.spike_diff;
 
 
 set(ax, 'XLim',[-inf inf]);
 set(ax, 'XtickMode','auto');
 set(ax, 'YtickMode','auto');
 
-plot(ax, time_vector, average_spike);
-xlabel(ax, 'Time (ms)')
-ylabel(ax, 'mV');
+plot(ax, average_spike(2:end), spike_diff);
+xlabel(ax, 'mV')
+ylabel(ax, 'mV/ms');
 
 hold(ax,'off');
