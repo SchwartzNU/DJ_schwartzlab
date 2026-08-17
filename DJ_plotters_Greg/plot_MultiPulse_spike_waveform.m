2769deb1eec0f40a85dde9008257dd114af1dfe3
Le Trung <38426554+trungtranle@users.noreply.github.com>
2026-08-17 15:24:29 -0500

    Update plot_MultiPulse_spike_waveform.m

--- DJ_plotters_Greg/plot_MultiPulse_spike_waveform.m
+++ DJ_plotters_Greg/plot_MultiPulse_spike_waveform.m
@@ -2,12 +2,13 @@ function required_fields = plot_MultiPulse_spike_waveform(R,ax)
 if nargin < 1
     required_fields = {'average_spike', ...
      'time_vector', ...
-     'spike_diff'};
+     'rheobase_mean'};
     return;
 end
 
 time_vector = R.time_vector;
 average_spike = R.average_spike;
+rheobase = R.rheobase_mean;
 
 
 set(ax, 'XLim',[-inf inf]);
@@ -17,5 +18,6 @@ set(ax, 'YtickMode','auto');
 plot(ax, time_vector, average_spike);
 xlabel(ax, 'Time (ms)')
 ylabel(ax, 'mV');
-
+t = sprintf('Rheobase %.3f pA', rheobase);
+subtitle(ax, t);
 hold(ax,'off');
